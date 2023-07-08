47b14846a5d1bc5f9c4199b79254d360d91d0818
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2023-07-08 12:27:11 +0900

    [unrated] Title: 수 조작하기 2, Time: 656.43 ms, Memory: 394 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,17 +1,43 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        int[] intControl = {1, -1, 10, -10};
-        char[] charControl = {'w','s','d','a'};
-        int n = numLog[0]; // 초기값
-        for(int i = 1; i < numLog.length; i++) {
-            for(int j = 0; j < intControl.length; j++) {
-                if(numLog[i] == (n + intControl[j])) {
-                    answer += charControl[j];
-                    n += intControl[j];
-                }
-            }
+
+        int temp = numLog[0];
+        for(int i=1; i<numLog.length; i++){
+
+           switch(numLog[i]-temp) {
+                case 1:
+                   answer += "w";
+                   break;
+                case -1:
+                   answer += "s";
+                   break;
+                case 10:
+                   answer += "d";
+                   break;
+                case -10:
+                   answer += "a";
+                   break;
+           }
+            temp = numLog[i];
         }
         return answer;
     }
 }
+// class Solution {
+//     public String solution(int[] numLog) {
+//         String answer = "";
+//         int[] intControl = {1, -1, 10, -10};
+//         char[] charControl = {'w','s','d','a'};
+//         int n = numLog[0]; // 초기값
+//         for(int i = 1; i < numLog.length; i++) {
+//             for(int j = 0; j < intControl.length; j++) {
+//                 if(numLog[i] == (n + intControl[j])) {
+//                     answer += charControl[j];
+//                     n += intControl[j];
+//                 }
+//             }
+//         }
+//         return answer;
+//     }
+// }
