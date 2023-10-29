bfa650e1b0d2837ea5eefd99fdebaa3d3f2e7fed
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2023-10-29 23:46:04 +0900

    [Bronze II] Title: 단어의 개수, Time: 260 ms, Memory: 20024 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+ 
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        
+        int count = 0;
+        
+        boolean isPrevStr = false;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ' && !isPrevStr) {
+                isPrevStr = true;
+                count++;
+            } else if (s.charAt(i) == ' ') {
+                isPrevStr = false;
+            }
+        }
+        
+        System.out.println(count);
+        br.close();
+    }
+}
