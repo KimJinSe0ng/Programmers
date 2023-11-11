999e064f81a5eda889d4a9e5ee5104ba5a1716bc
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2023-11-11 23:20:44 +0900

    [Bronze II] Title: 분해합, Time: 144 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,28 @@
+import javax.annotation.processing.SupportedSourceVersion;
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int ans = 0;  //답
+        for (int i = 1; i < n; i++) {
+            int num = function(i);
+            if (num == n) { //분해합이 n과 같으면 i는 n의 생성자가 된다
+                ans = i;
+                break;
+            }
+        }
+        System.out.print(ans);
+    }
+
+    //분해합 구하는 함수
+    static int function(int n) {
+        int ret = n;
+        while (n != 0) {
+            ret += n % 10;
+            n /= 10;
+        }
+        return ret;
+    }
+}
