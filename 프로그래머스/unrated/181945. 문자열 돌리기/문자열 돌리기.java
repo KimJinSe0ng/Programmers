92ae8e4623f9bf64ea7e38cd69a18193f3457cb7
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2023-11-23 21:38:22 +0900

    [level unrated] Title: 문자열 돌리기, Time: 177.33 ms, Memory: 65.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        for(int i = 0; i < a.length(); i++) {
-            System.out.println(a.charAt(i));
+        String str = sc.next();
+        for(char c : str.toCharArray()) {
+            System.out.println(c);
         }
     }
 }
