50d2c784a84d328230e0cb4e36904207f7217461
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2023-11-24 15:17:57 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,34 +1,56 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String args[]) {
-    	Scanner sc = new Scanner(System.in);
-    	int N = sc.nextInt(); // 카드의 개수
-    	int M = sc.nextInt(); // 딜러가 외친 숫자
-    	int [] card = new int [N];
-    	
-    	for(int i=0; i<N; i++) {
-    		card[i] = sc.nextInt();
-    	}
-    	int mini = 0;
-    	// 3중 for문으로 카드 3개 고르기
-    	for(int i=0; i<=N-3; i++) { // 첫 번째로 고르는 카드
-    		for(int j=i+1; j<=N-2; j++) { // 두 번째로 고르는 카드
-    			for(int k=j+1; k<N; k++) { // 세 번째로 고르는 카드
-    				int hap = card[i]+card[j]+card[k];
-    				// 세 카드의 합과 M이 같다면 hap을 출력하고 바로 종료
-    				if(hap == M) {
-    					System.out.println(hap);
-    					return;
-    				}
-    				// hap이 이전 합보다 크고 M보다 작다면 (M에 더 가까운것임)
-    				// mini에 hap 저장
-    				if(mini < hap && hap < M) {
-    					mini = hap;
-    				}
-    			}
-    		}
-    	}
-    	System.out.println(mini);
+    public static void main(String[] args) throws IOException {
+        //각 카드에는 양의 정수, 딜러는 N장의 카드를 모두 숫자가 보이도록 바닥에 놓는다.
+        //딜러는 숫자 M을 외친다.
+        //플레이어는 N장의 카드 중에서 3장의 카드를 고른다. 고른 카드의 합은 M을 넘지 않으며 최대한 가까이 만들어야 한다.
+        //N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] cards = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < cards.length; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        int result = search(cards, N, M);
+        System.out.println(result);
+    }
+
+    static int search(int[] arr, int N, int M) {
+        int result = 0;
+
+        // 3개를 고르기 때문에 첫번째 카드는 N - 2 까지만 순회
+        for (int i = 0; i < N - 2; i++) {
+
+            // 두 번째 카드는 첫 번째 카드 다음부터 N - 1 까지만 순회
+            for (int j = i + 1; j < N - 1; j++) {
+
+                // 세 번째 카드는 두 번째 카드 다음부터 N 까지 순회
+                for (int k = j + 1; k < N; k++) {
+
+                    // 3개 카드의 합 변수 temp
+                    int temp = arr[i] + arr[j] + arr[k];
+
+                    // M과 세 카드의 합이 같다면 temp return 및 종료
+                    if (M == temp) {
+                        return temp;
+                    }
+
+                    // 세 카드의 합이 이전 합보다 크면서 M 보다 작을 경우 result 갱신
+                    if(result < temp && temp < M) {
+                        result = temp;
+                    }
+                }
+            }
+        }
+
+        // 모든 순회를 마치면 result 리턴
+        return result;
     }
 }
