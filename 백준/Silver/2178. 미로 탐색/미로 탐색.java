99566163e5ac9bb7133c7ceaced29a4241cbfa21
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2023-12-05 23:38:52 +0900

    [Silver I] Title: 미로 탐색, Time: 160 ms, Memory: 15532 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,56 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int map[][];
-    static boolean visited[][];
-    static int dx[] = {0, 0, 1, -1};
-    static int dy[] = {1, -1, 0, 0};
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        N = scan.nextInt();
-        M = scan.nextInt();
-
-        map = new int[N + 1][M + 1];
-        visited = new boolean[N + 1][M + 1];
-
-        for (int i = 0; i < N; i++) {
-            String a = scan.next();
-            for (int j = 0; j < M; j++) {
-                map[i][j] = a.charAt(j) - '0';
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {-1, 1, 0, 0};
+    static int n; //행
+    static int m; //열
+    static int[][] a;
+    static boolean[][] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        a = new int[n][m];
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            for (int j = 0; j < m; j++) {
+                a[i][j] = Integer.parseInt(line.substring(j, j + 1));
             }
         }
-        bfs();
-        System.out.print(map[N - 1][M - 1]);
+
+        bfs(0, 0);
+        System.out.println(a[n - 1][m - 1]);
     }
 
-    public static void bfs() {
+    private static void bfs(int i, int j) {
         Queue<int[]> queue = new LinkedList<>();
-
-        int x = 0;
-        int y = 0;
-        visited[x][y] = true;
-        queue.add(new int[]{x, y});
+        queue.offer(new int[]{i, j});
+        visited[i][j] = true;
         while (!queue.isEmpty()) {
-            int xy[] = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                x = xy[0] + dx[i];
-                y = xy[1] + dy[i];
-                if (x < 0 || y < 0 || x > N || y > M || visited[x][y] == true || map[x][y] == 0) {
-                    continue;
-                } else {
-                    visited[x][y] = true;
-                    queue.add(new int[]{x, y});
-                    map[x][y] = map[xy[0]][xy[1]] + 1;
+            int now[] = queue.poll();
+            for (int k = 0; k < 4; k++) {
+                int x = now[0] + dx[k];
+                int y = now[1] + dy[k];
+                if (x >= 0 && y >= 0 && x < n && y < m) {
+                    if (a[x][y] != 0 && !visited[x][y]) {
+                        visited[x][y] = true;
+                        a[x][y] = a[now[0]][now[1]] + 1;
+                        queue.add(new int[]{x, y});
+                    }
                 }
             }
         }
-
-
     }
 }
