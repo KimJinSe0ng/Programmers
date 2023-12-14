5fa29e65a2dfa82c3c0de7a7c3458f318a4427b9
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2023-12-14 15:15:41 +0900

    [Bronze III] Title: 이 문제는 D2 입니다., Time: 256 ms, Memory: 18040 KB -BaekjoonHub

---
+++ 백준/Bronze/23303. 이 문제는 D2 입니다．/이 문제는 D2 입니다．.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String regExp = "[dD][2]";
+        String line = sc.nextLine();
+        Matcher matcher = Pattern.compile(regExp).matcher(line);
+        if (matcher.find()) {
+            System.out.println("D2");
+        } else {
+            System.out.println("unrated");
+        }
+    }
+}
