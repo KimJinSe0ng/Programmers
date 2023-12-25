47a8d40b91bf3c64af18a403ca04d08d924acbb1
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2023-12-25 22:01:30 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1128 ms, Memory: 91900 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,60 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer>[] A;
+    static boolean visited[];
+    static int[] order;
+    static int count = 1;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        A = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            A[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[e].add(s);
+            A[s].add(e);
+        }
+        for (int i = 1; i < N + 1; i++) {
+            Collections.sort(A[i], Collections.reverseOrder());
+        }
+        order = new int[N + 1];
+        bfs(R);
+        for (int i = 1; i <= N; i++) {
+            sb.append(order[i]).append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+
+    private static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(v);
+        visited[v] = true;
+        order[v] = count++;
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+//            visited[count] = now;
+//            count++;
+            for (int newNode : A[now]) {
+                if (!visited[newNode]) {
+                    queue.add(newNode);
+                    visited[newNode] = true;
+                    order[newNode] = count++;
+                }
+            }
+        }
+    }
+}
