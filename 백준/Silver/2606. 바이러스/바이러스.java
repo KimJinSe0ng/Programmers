b63ebf986f489981c02583e7e2ccf3a3483bf1d7
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-01-15 11:06:11 +0900

    [Silver III] Title: 바이러스, Time: 212 ms, Memory: 17804 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,42 +2,39 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static ArrayList<Integer>[] a;
-    static boolean visit[];	// 정점 탐색여부 체크
-    static int n, m, v;		// 정점, 간선, 시작 정점
-    static int count = 0;	// 정점과 연결된 바이러스 걸리는 컴퓨터 수
+    static ArrayList<Integer>[] A;
+    static boolean visited[];
+    static int count = 0;
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        n = scan.nextInt();	// 컴퓨터 수(정점)
-        m = scan.nextInt();	// 연결된 컴퓨터 쌍의 수(간선)
-        v = 1;	// 탐색 시장할 정점의 번호
-        a = new ArrayList[n+1];	// 인덱스 편의상 n+1설정, 0번째 요소는 사용 X
-        visit = new boolean[n+1];
-        for(int i=1; i<=n; i++) {
-            a[i] = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        A = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            A[i] = new ArrayList<>();
         }
-
-        for(int i=0; i<m; i++) {
-            int u = scan.nextInt();	// 간선으로 이어진 정점1
-            int v = scan.nextInt();	// 정점1과 간선으로 이어진 정점2
-            a[u].add(v);
-            a[v].add(u);
+        for (int i = 0; i < m; i++) {
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+            A[s].add(e);
+            A[e].add(s);
         }
-
-        System.out.println(dfs(v));
-
-        scan.close();
+        dfs(1);
+        System.out.println(count);
     }
 
-    public static int dfs(int i) {
-        visit[i] = true;
-        for(int k : a[i]) {
-            if(visit[k] == false) {
-                count ++;
-                dfs(k);
+    static void dfs(int v) {
+        if (visited[v]) {
+            return;
+        }
+        visited[v] = true;
+        for (int i : A[v]) {
+            if (visited[i] == false) {
+                count++;
+                dfs(i);
             }
         }
-        return count;
     }
 }
