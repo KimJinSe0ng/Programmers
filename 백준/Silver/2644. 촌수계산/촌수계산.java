87ebd995d5322c1ade6aa3f566edfad798214083
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-01-19 22:30:31 +0900

    [Silver II] Title: 촌수계산, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,50 +1,58 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
-	
-	static List<Integer>[] relation;
-	static boolean[] checked;
-	static int res = -1;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int n = Integer.parseInt(br.readLine());
-		relation = new ArrayList[n+1];
-		checked = new boolean[n+1];
-		for(int i=1; i<n+1; i++) {
-			relation[i] = new ArrayList<>();
-		}
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		int l = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<l; i++) {
-			st = new StringTokenizer(br.readLine());
-			int p = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			relation[p].add(c);
-			relation[c].add(p);
-		}
-		
-		dfs(x,y, 0);
-		System.out.println(res);
-	}
-	
-	static void dfs(int start, int end, int cnt) {
-		if(start == end) {
-			res = cnt;
-			return; 
-		}
-		
-		checked[start] = true;
-		for(int i=0; i<relation[start].size(); i++) { 
-			int next = relation[start].get(i);
-			if(!checked[next]) {
-				dfs(next, end, cnt+1);
-			}
-		}
-	}
+
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+
+        int num = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        int loop = Integer.parseInt(br.readLine());
+
+        List<List<Integer>> family = new ArrayList<>();
+        for(int i = 0; i < num + 1 ; i ++) {
+            family.add(new ArrayList<>());
+        }
+
+        for (int i=0; i<loop; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            family.get(a).add(b);
+            family.get(b).add(a);
+        }
+        int[] visited = new int[num+1];
+        bfs(visited, start, family);
+        System.out.println(visited[end] == 0 ? -1 : visited[end]);
+    }
+
+    private static void bfs(int[] visited, int start, List<List<Integer>> list){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = 0;
+
+        while(!queue.isEmpty()){
+            Integer current = queue.poll();
+            int current_dist = visited[current];
+
+            for(Integer next: list.get(current)){
+                if (visited[next] == 0 || visited[next] >= current_dist + 1) {
+                    queue.add(next);
+                    visited[next] = current_dist + 1;
+                }
+            }
+        }
+    }
 }
