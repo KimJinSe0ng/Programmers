8181491a4dd8bcc691941c67e5738f1244a5c00a
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-02-14 12:45:19 +0900

    [Silver V] Title: 수들의 합 5, Time: 164 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,24 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+public class Main { //2018
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
         int count = 1;
         int start_index = 1;
         int end_index = 1;
         int sum = 1;
-        while (end_index != N) {
-            if (sum == N) {
+        while (end_index != n) {
+            if (sum == n) { //현재 연속 합이 n과 같은 경우
                 count++;
                 end_index++;
-                sum = sum + end_index;
-            } else if (sum > N) {
-                sum = sum - start_index;
+                sum += end_index;
+            } else if (sum > n) { //현재 연속 합이 n보다 더 큰 경우
+                sum -= start_index;
                 start_index++;
-            } else {
+            } else { //현재 연속 합이 n보다 작은 경우
                 end_index++;
-                sum = sum + end_index;
+                sum += end_index;
             }
         }
         System.out.println(count);
