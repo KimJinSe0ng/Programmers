8d2f98b54300289371439cc8573996e154fbda46
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-03-31 12:26:31 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 16544 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,57 +1,50 @@
 import java.util.*;
 import java.io.*;
-
-public class Main {
-    static int K;
-    static boolean[] visited;
-    static int min = Integer.MAX_VALUE;
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        visited = new boolean[100_001];
-
-        bfs(N);
-
-        System.out.println(min);
-    }
-
-    static void bfs(int val){
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(val, 0));
-        visited[val] = true;
-
-        while(!q.isEmpty()){
-            Node a = q.poll();
-
-            if(a.x == K)
-                min = Math.min(min, a.time);
-
-            if(a.x*2<=100_000 && !visited[a.x*2]){
-                q.offer(new Node(a.x*2, a.time));
-                visited[a.x*2] = true;
-            }
-
-            if(a.x-1>=0 && !visited[a.x-1]){
-                q.offer(new Node(a.x-1, a.time + 1));
-                visited[a.x-1] = true;
-            }
-
-            if(a.x+1<=100_000 && !visited[a.x+1]){
-                q.offer(new Node(a.x+1, a.time + 1));
-                visited[a.x+1] = true;
-            }
-        }
+ 
+class Node{
+    int where;
+    int time;
+ 
+    public Node(int where, int time) {
+        this.where = where;
+        this.time = time;
     }
 }
-class Node{
-    int x, time;
-
-    Node(int x, int time){
-        this.x = x;
-        this.time = time;
+ 
+public class Main {
+    static int n,k;
+    static boolean [] check;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        n= Integer.parseInt(s[0]);
+        k = Integer.parseInt(s[1]);
+        check= new boolean[100002];
+        solve();
+    }
+ 
+    public static void solve(){
+        Queue<Node> queue =new ArrayDeque<>();
+        queue.add(new Node(n, 0));
+        while(!queue.isEmpty()){
+            Node poll = queue.poll();
+            if(poll.where==k){
+                System.out.println(poll.time);
+                return;
+            }
+ 
+            if(poll.where*2<=100001 && !check[poll.where*2]){
+                queue.add(new Node(poll.where*2, poll.time));
+                check[poll.where*2]=true;
+            }
+            if(poll.where-1>=0 &&!check[poll.where-1]){
+                queue.add(new Node(poll.where-1, poll.time+1));
+                check[poll.where-1]=true;
+            }
+            if(poll.where+1<=100001 && !check[poll.where+1]){
+                queue.add(new Node(poll.where+1, poll.time+1));
+                check[poll.where+1]=true;
+            }
+        }
     }
 }
