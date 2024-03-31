228d7517f33536ca7d986c7d9f8efb0d8358daa4
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-03-31 13:11:23 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 684 ms, Memory: 141104 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,88 +2,84 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[][] arr;
 
-    static int n, m;
-    static int[][] board;
-    static boolean[][][] visited;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+    static int[][] dist;    
+    static boolean[][][] visit;    
 
-    static class Point {
-        int x;
-        int y;
-        boolean destroyed;
-        int cnt;
+    public static int[] dx = {-1, 0, 1, 0};
 
-        public Point(int x, int y, boolean destroyed, int cnt) {
-            this.x = x;
-            this.y = y;
-            this.destroyed = destroyed;
-            this.cnt = cnt;
-        }
-    }
+    public static int[] dy = {0, 1, 0, -1};
+    static int result = -1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        dist = new int[N][M];
+        visit = new boolean[2][N][M];
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        board = new int[n][m];
-        visited = new boolean[n][m][2];
-
-        for (int i = 0; i < n; i++) {
-            char[] charArray = br.readLine().toCharArray();
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Character.getNumericValue(charArray[j]);
-            }
+ 
+        if (N - 1 == 0 && M - 1 == 0) {
+            System.out.print(1);
+            System.exit(0);
         }
 
-        System.out.println(bfs());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+        BFS();
+        System.out.println(result);
+
     }
 
-    public static int bfs() {
-        Queue<Point> queue = new LinkedList<>();
+    private static void BFS() {
+        LinkedList<int[]> que = new LinkedList<>();
+        que.offer(new int[]{0, 0, 0});
+        arr[0][0] = -1;
 
-        queue.offer(new Point(0, 0, false, 1));
-        visited[0][0][0] = true;
+        while (!que.isEmpty()) {
+            int[] poll = que.poll();
+            int x = poll[0];
+            int y = poll[1];
+            int z = poll[2]; 
 
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int nowX = x + dx[i];
+                int nowY = y + dy[i];
 
-            if (point.x == n - 1 && point.y == m - 1) {
-                return point.cnt;
-            }
-
-            for (int d = 0; d < 4; d++) {
-                int newX = point.x + dx[d];
-                int newY = point.y + dy[d];
-
-                if (newX < 0 || newX >= n || newY < 0 || newY >= m) {
+                if (nowX < 0 || nowY < 0 || N <= nowX || M <= nowY) {
                     continue;
                 }
 
-                if (point.destroyed) {
-                    if (board[newX][newY] == 0 && !visited[newX][newY][1]) {
-                        visited[newX][newY][1] = true;
-                        queue.offer(new Point(newX, newY, true, point.cnt + 1));
+                if (arr[nowX][nowY] == 1) {
+                    if (z == 0 && !visit[1][nowX][nowY]) {
+                        visit[z][nowX][nowY] = true;   
+                        dist[nowX][nowY] = dist[x][y] + 1; 
+                        que.offer(new int[]{nowX, nowY, 1});
                     }
                 } else {
-                    if (board[newX][newY] == 1) {
-                        visited[newX][newY][1] = true;
-                        queue.offer(new Point(newX, newY, true, point.cnt + 1));
-                    } else if (!visited[newX][newY][0]){
-                        visited[newX][newY][0] = true;
-                        queue.offer(new Point(newX, newY, false, point.cnt + 1));
+                    if (!visit[z][nowX][nowY]) {
+                        visit[z][nowX][nowY] = true;   
+                        dist[nowX][nowY] = dist[x][y] + 1; 
+                        que.offer(new int[]{nowX, nowY, z}); 
                     }
+                }
+
+                if (nowX == N - 1 && nowY == M - 1) {
+                    result = dist[nowX][nowY] + 1;
+                    return;
                 }
             }
         }
-
-        return -1;
     }
 }
