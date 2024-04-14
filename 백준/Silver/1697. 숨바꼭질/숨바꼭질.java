bb1b464322d43daf65a5b7a9a0f977759823c740
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-04-14 13:31:04 +0900

    [Silver I] Title: 숨바꼭질, Time: 168 ms, Memory: 20000 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,70 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-    static int[] movable = {-1, 1, 2}; //X-1, X+1, X*2
-//    static boolean visited[];
-    static int visited[];
-//    static int[] move;
-    public static void main(String[] args) {
-        /**
-         * 수빈이는 현재 점 N, 동생은 점 K에 있다.
-         * 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 or X+1로 이동
-         * 순간이동의 경우 1초 후에 2*X로 이동
-         * 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인가?
-         */
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
-        visited = new int[100001];
-//        visited = new boolean[100001];
-//        move = new int[100001];
-        if (N == K) {
-            System.out.println("0");
-        } else {
-            BFS(N);
-        }
-//        System.out.println(move[K]);
+    static int n, k;
+    static boolean[] visited;
+    static int[] time;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        visited = new boolean[100001];
+        time = new int[100001];
+        bfs(n);
+        System.out.println(time[k]);
     }
 
-    private static void BFS(int x) {
+    private static void bfs(int pos) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(x);
-//        visited[x] = true;
-//        move[x] = 1;
-        visited[x] = 1;
+        queue.add(pos);
+        visited[pos] = true;
+
         while (!queue.isEmpty()) {
-            int now = queue.poll();
+            int cur = queue.poll();
+            if (cur == k) { //예외 : 시작 부터 수빈이를 만나면
+                return;
+            }
             for (int i = 0; i < 3; i++) {
-                int nx;
-                if (i != 2) {
-                    nx = now + movable[i];
-                } else {
-                    nx = now * movable[i];
-                }
-                if (nx == K) {
-//                    System.out.println("move[now] = " + move[now]);
-//                    move[nx] = move[now];
-                    System.out.println(visited[now]);
-                    return;
-                }
-                if (nx >= 0 && nx <= 100000) {
-                    if (visited[nx] == 0) {
-//                        visited[nx] = 1;
-                        queue.add(nx);
-//                        move[nx] = move[now] + 1;
-                        visited[nx] = visited[now] + 1;
+                int moved = move(cur, i);
+                if (moved >= 0 && moved <= 100000) {
+                    if (!visited[moved]) {
+                        visited[moved] = true;
+                        time[moved] = time[cur] + 1;
+                        queue.add(moved);
                     }
                 }
-//                if (nx == K) {
-//                    System.out.println("nx = " + nx);
-//                    System.out.println("move[nx] = " + move[nx]);
-//                    System.out.println("now = " + now);
-//                    return;
-//                }
             }
         }
+
+    }
+
+    private static int move(int now, int index) {
+        switch (index) {
+            case 0:
+                now -= 1;
+                break;
+            case 1:
+                now += 1;
+                break;
+            case 2:
+                now *= 2;
+                break;
+        }
+        return now;
     }
 }
