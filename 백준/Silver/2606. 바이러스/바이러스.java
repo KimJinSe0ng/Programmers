2dfc7ab4457a943664c6665890209d5b2b9fd566
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-04-14 10:18:46 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,39 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] A;
-    static boolean visited[];
+    static int N, M;
+    static ArrayList<Integer>[] network;
+    static boolean[] visited;
     static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        network = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        A = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            A[i] = new ArrayList<>();
+        for (int i = 1; i < N + 1; i++) {
+            network[i] = new ArrayList<>();
         }
-        for (int i = 0; i < m; i++) {
-            int s = sc.nextInt();
-            int e = sc.nextInt();
-            A[s].add(e);
-            A[e].add(s);
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            network[s].add(e);
+            network[e].add(s);
         }
+
         dfs(1);
         System.out.println(count);
     }
 
-    static void dfs(int v) {
-        if (visited[v]) {
-            return;
-        }
-        visited[v] = true;
-        for (int i : A[v]) {
-            if (visited[i] == false) {
+    private static void dfs(int node) {
+        visited[node] = true;
+        for (int v : network[node]) {
+            if (!visited[v]) {
+                visited[v] = true;
                 count++;
-                dfs(i);
+                dfs(v);
             }
         }
     }
