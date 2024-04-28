c0c5da3aaadf0a30e752e170dc04fd9f6f2668d8
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-04-28 12:52:06 +0900

    [Gold V] Title: ABCDE, Time: 288 ms, Memory: 23096 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -4,32 +4,31 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
-
+public class Main { //13023
+    static int N, M;
     static ArrayList<Integer>[] A;
-    static boolean visited[];
+    static boolean[] visited;
     static boolean arrive;
-
     public static void main(String[] args) throws IOException {
-        //주어진 모든 노드에 DFS를 수행하고 재귀의 깊이가 5이상(5개의 노드가 재귀 형태로 연결)이면 1, 아니면 0
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        A = new ArrayList[n];
-        visited = new boolean[n];
-        arrive = false;
-        for (int i = 0; i < n; i++) {
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        A = new ArrayList[N];
+        visited = new boolean[N];
+        for (int i = 0; i < N; i++) {
             A[i] = new ArrayList<>();
         }
-        for (int i = 0; i < m; i++) {
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             A[s].add(e);
             A[e].add(s);
         }
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < N; i++) {
             dfs(i, 1); //depth 1부터 시작
             if (arrive) {
                 break;
@@ -40,19 +39,21 @@ public class Main {
         } else {
             System.out.println("0");
         }
+
     }
 
-    static void dfs(int now, int depth) {
-        if (depth == 5 || arrive) { //depth가 5가 되면 종료
+    private static void dfs(int now, int depth) {
+//        System.out.println("now = " + now + " depth = " + depth);
+        if (depth == 5 || arrive) {
             arrive = true;
             return;
         }
         visited[now] = true;
-        for (int i : A[now]) {
+        for (int i : A[now]) { //현재 노드의 연결 노드 중 방문하지 않은 노드 탐색
             if (!visited[i]) {
-                dfs(i, depth + 1); //재귀 호출이 될 때마다 depth를 1씩 증가
+                dfs(i, depth + 1);
             }
         }
-        visited[now] = false;
+        visited[now] = false; //노드가 연결되어 있을 수 있지만 true 상태이면 방문하지 않는다. 즉, 관계 파악할 수 없음
     }
 }
