a75729110babbc7d8a6356de0a6ae34de690ced3
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-05-03 19:39:44 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 376 ms, Memory: 20212 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -1,30 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-
-        static int count = 0;
-
-        public static int recursion(String s, int l, int r){
-            count++;
-            if(l >= r) return 1;
-            else if(s.charAt(l) != s.charAt(r)) return 0;
-            else return recursion(s, l+1, r-1);
+    static int result;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            String S = st.nextToken();
+            result = 0;
+            System.out.println(isPalindrome(S) + " " + result);
         }
+    }
 
-        public static int isPalindrome(String s){
-            return recursion(s, 0, s.length()-1);
-        }
-
-        public static void main(String[] args){
-        
-        Scanner sc = new Scanner(System.in);
-		int testCase = sc.nextInt();		
-
-        for(int i = 0 ; i < testCase ; i++){
-            count = 0;
-            String str = sc.next();
-            System.out.println(isPalindrome(str) + " " + count);
-        }
-        sc.close();
-        }
+    private static int recursion(String s, int l, int r){
+        result++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    private static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
 }
