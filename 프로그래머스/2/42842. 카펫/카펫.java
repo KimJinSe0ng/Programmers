710ef4970c798c6928d5f1149d83293f012dce3d
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-05-04 16:51:42 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        //가능한 모든 width 와 height 검사
-        for (int width = 3; width <= 5000; width++) {
-            for (int height = 3; height <= width; height++) {
-                //조건 검사
+        for (int width = 3; width <= 5000; width++) { //가능한 모든 가로, 세로 검사
+            for (int height = 3; height <= width; height++) { //세로는 가로 길이 이하
                 int boundary = (width + height - 2) * 2; //경계 격자 개수
                 int center = width * height - boundary; //내부 격자 개수
                 if (brown == boundary && yellow == center) {
