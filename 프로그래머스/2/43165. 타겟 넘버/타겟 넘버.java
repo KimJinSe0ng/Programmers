a8d6679c5dd32fed540c576c47b42bfa10aec5e8
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-05-05 17:50:43 +0900

    [level 2] Title: 타겟 넘버, Time: 0.35 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,39 +1,18 @@
-import java.util.*;
-
 class Solution {
-    
-    private static class State {
-        public final int index; //연산자를 결정할 인덱스 위치
-        public final int acc; //현재까지 결정된 연산자로 결정된 값
-
-        State(int index, int acc) {
-            this.index = index;
-            this.acc = acc;
-        }
-    }
-    
     public int solution(int[] numbers, int target) {
-        Stack<State> s = new Stack<>();
-        s.push(new State(0, 0));
-                    
-        int count = 0;
-        
-        while (!s.isEmpty()) {
-            State state = s.pop();
-            
-            //state 처리, 전이
-            if (state.index == numbers.length) {
-                if (state.acc == target) count++;
-                continue;
-            }
-            
-            //상태 전이
-            //+를 선택한 경우
-            s.push(new State(state.index + 1, state.acc - numbers[state.index]));
-            //-를 선택한 경우
-            s.push(new State(state.index + 1, state.acc + numbers[state.index]));
-        }
-        
-        return count;
+    return dfs(numbers, target, 0, 0);
+    }
+
+    private int dfs(int[] numbers, int target, int index, int acc) {
+    if (index == numbers.length) {
+        return acc == target ? 1 : 0;
+    }
+
+    int count = 0;
+    count += dfs(numbers, target, index + 1, acc - numbers[index]);
+    count += dfs(numbers, target, index + 1, acc + numbers[index]);
+    
+    return count;
     }
 }
+
