78686073377092c296cee222d565a1e2d62d996b
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-05-07 11:19:31 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2892 ms, Memory: 366412 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,64 +1,58 @@
 import java.util.*;
 
 public class Main {
-
-    // 도시의 개수, 도로의 개수, 거리 정보, 출발 도시 번호
-    public static int n, m, k, x;
-    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-    // 모든 도시에 대한 최단 거리 초기화
-    public static int[] d = new int[300001];
+    static int N, M, K, X;
+    static ArrayList<Integer>[] A;
+    static int[] visited = new int[300001];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        K = sc.nextInt();
+        X = sc.nextInt();
 
-        n = sc.nextInt();
-        m = sc.nextInt();
-        k = sc.nextInt();
-        x = sc.nextInt();
-
-        // 그래피 및 최단 거리 테이블 초기화
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<Integer>());
-            d[i] = -1;
+        A = new ArrayList[N + 1];
+//        visited = new int[N + 1];
+//        Arrays.fill(visited, -1);
+        for (int i = 0; i <= N; i++) {
+            A[i] = new ArrayList<>();
+            visited[i] = -1;
         }
 
-        // 모든 도로 정보 입력 받기
-        for (int i = 0; i < m; i++) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            graph.get(a).add(b);
+        for (int i = 0; i < M; i++) {
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+            A[s].add(e);
+            //A[e].add(s);
         }
 
-        // 출발 도시까지의 거리는 0으로 설정
-        d[x] = 0;
+        bfs(X);
+    }
 
-        // 너비 우선 탐색(BFS) 수행
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.offer(x);
+    private static void bfs(int v) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(v);
+        visited[v] = 0;
         while (!q.isEmpty()) {
             int now = q.poll();
-            // 현재 도시에서 이동할 수 있는 모든 도시를 확인
-            for (int i = 0; i < graph.get(now).size(); i++) {
-                int nextNode = graph.get(now).get(i);
-                // 아직 방문하지 않은 도시라면
-                if (d[nextNode] == -1) {
-                    // 최단 거리 갱신
-                    d[nextNode] = d[now] + 1;
-                    q.offer(nextNode);
+            for (int next : A[now]) {
+                if (visited[next] == -1) {
+                    visited[next] = visited[now] + 1;
+                    q.add(next);
                 }
             }
         }
 
-        // 최단 거리가 K인 모든 도시의 번호를 오름차순으로 출력
         boolean check = false;
-        for (int i = 1; i <= n; i++) {
-            if (d[i] == k) {
+        for (int i = 1; i <= N; i++) {
+            if (visited[i] == K) {
                 System.out.println(i);
                 check = true;
             }
         }
 
-        // 만약 최단 거리가 K인 도시가 없다면, -1 출력
         if (!check) System.out.println(-1);
     }
+
 }
