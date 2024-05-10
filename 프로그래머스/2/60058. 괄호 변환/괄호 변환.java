76d79a6746b8ff83a8a3d2e309b094685956f224
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-05-10 10:06:43 +0900

    [level 2] Title: 괄호 변환, Time: 2.55 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,66 @@
+class Solution {
+    // "균형잡힌 괄호 문자열"의 인덱스 반환
+    public int balancedIndex(String p) {
+        int count = 0; // 왼쪽 괄호의 개수
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '(') count += 1;
+            else count -= 1;
+            if (count == 0) return i;
+        }
+        return -1;
+    }
+
+    // "올바른 괄호 문자열"인지 판단
+    public boolean checkProper(String p) {
+        int count = 0; // 왼쪽 괄호의 개수
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '(') count += 1;
+            else {
+                if (count == 0) { // 쌍이 맞지 않는 경우에 false 반환
+                    return false;
+                }
+                count -= 1;
+            }
+        }
+        return true; // 쌍이 맞는 경우에 true 반환
+    }
+
+    public String solution(String p) {
+        String answer = "";
+        if (p.equals("")) return answer;
+        int index = balancedIndex(p);
+        String u = p.substring(0, index + 1);
+        String v = p.substring(index + 1);
+        // "올바른 괄호 문자열"이면, v에 대해 함수를 수행한 결과를 붙여 반환
+        if (checkProper(u)) {
+            answer = u + solution(v);
+        }
+        // "올바른 괄호 문자열"이 아니라면 아래의 과정을 수행
+        else {
+            answer = "(";
+            answer += solution(v);
+            answer += ")";
+            u = u.substring(1, u.length() - 1); // 첫 번째와 마지막 문자를 제거
+            String temp = "";
+            for (int i = 0; i < u.length(); i++) {
+                if (u.charAt(i) == '(') temp += ")";
+                else temp += "(";
+            }
+            answer += temp;
+        }
+        return answer;
+    }
+}
+
+/*
+1. 입력이 빈 문자열인 경우, 빈 문자열을 반환합니다. 
+2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다. 
+3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다. 
+  3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다. 
+4. 문자열 u가 "올바른 괄호 문자열"이 아니라면 아래 과정을 수행합니다. 
+  4-1. 빈 문자열에 첫 번째 문자로 '('를 붙입니다. 
+  4-2. 문자열 v에 대해 1단계부터 재귀적으로 수행한 결과 문자열을 이어 붙입니다. 
+  4-3. ')'를 다시 붙입니다. 
+  4-4. u의 첫 번째와 마지막 문자를 제거하고, 나머지 문자열의 괄호 방향을 뒤집어서 뒤에 붙입니다. 
+  4-5. 생성된 문자열을 반환합니다.
+*/
