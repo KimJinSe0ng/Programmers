e22bda3978ef86cd4883d1057461540dacf58fc6
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-05-19 09:21:31 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18092 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,26 +1,26 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
+public class Main { //1463
+    static int n;
+    static int[] D;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int[] dp = new int[N+1];
-
-        dp[1] = 0;
-        for (int i = 2; i < N+1; i++)
-        {
-            dp[i] = dp[i-1]+1; // 1을 뺀다
-            if (i % 2 == 0 && dp[i] > dp[i/2]+1) // 2로 나누어 떨어지면, 2로 나눈다.
-            {
-                dp[i] = dp[i/2]+1;
+        n = Integer.parseInt(st.nextToken());
+        D = new int[n + 1];
+        D[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            D[i] = D[i - 1] + 1; //-1연산
+            if (i % 2 == 0) {
+                D[i] = Math.min(D[i], D[i / 2] + 1); //나누기 2 연산
             }
-            if (i % 3 == 0 && dp[i] > dp[i/3]+1) // 3으로 나누어 떨어지면, 3으로 나눈다.
-            {
-                dp[i] = dp[i/3]+1;
+            if (i % 3 == 0) {
+                D[i] = Math.min(D[i], D[i / 3] + 1); //나누기 3연산
             }
         }
-        System.out.println(dp[N]);
+        System.out.println(D[n]);
     }
 }
