9c4feecb5c3c6a7b804d3fc6eb760bad3b376622
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-05-22 21:42:04 +0900

    [Gold V] Title: 신기한 소수, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,39 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        DFS(2, 1); //일의 자리 소수는 2,3,5,7이므로 4개 수에서만 시작
-        DFS(3, 1);
-        DFS(5, 1);
-        DFS(7, 1);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
     }
-    static void DFS(int number, int jarisu) {
-        if (jarisu == N) {
-            if (isPrime(number)) {
-                System.out.println(number);
+
+    private static void dfs(int num, int digit) {
+        if (digit == N) {
+            if (isPrime(num)) {
+                System.out.println(num);
             }
             return;
         }
-        for (int i = 1; i < 10; i++) {
-            if (i % 2 == 0) { //짝수라면 더 이상 탐색할 필요 없음
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
                 continue;
             }
-            if (isPrime(number * 10 + i)) { //소수라면 재귀 함수로 자릿수를 늘림
-                DFS(number * 10 + i, jarisu + 1);
+            if (isPrime(num * 10 + i)) {
+                dfs(num * 10 + i, digit + 1);
             }
         }
     }
 
-    static boolean isPrime(int num) {
-        for (int i = 2; i <= num / 2; i++) {
+    private static boolean isPrime(int num) {
+        for (int i = 2; i * i <= num; i++) { //방법1
             if (num % i == 0) {
                 return false;
             }
         }
         return true;
+//        for (int i = 2; i <= num / 2; i++) { //방법2
+//            if (num % i == 0) {
+//                return false;
+//            }
+//        }
+//        return true;
     }
 }
