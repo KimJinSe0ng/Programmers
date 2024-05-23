a6211772fb4c093fc088769eaec9f61a5fa2e689
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-05-23 17:55:26 +0900

    [Gold V] Title: ABCDE, Time: 284 ms, Memory: 20936 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -4,11 +4,11 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main { //13023
+public class Main {
     static int N, M;
-    static ArrayList<Integer>[] A;
     static boolean[] visited;
-    static boolean arrive;
+    static ArrayList<Integer>[] A;
+    static boolean isExist = false;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,6 +16,7 @@ public class Main { //13023
         M = Integer.parseInt(st.nextToken());
         A = new ArrayList[N];
         visited = new boolean[N];
+
         for (int i = 0; i < N; i++) {
             A[i] = new ArrayList<>();
         }
@@ -28,30 +29,30 @@ public class Main { //13023
             A[e].add(s);
         }
 
-        for (int i = 0; i < N; i++) {
-            dfs(i, 1); //depth 1부터 시작
-            if (arrive) {
+        for (int i = 0; i < N; i++) { //0부터 시작하여 N까지 찾아보기
+            DFS(i, 0);
+            if (isExist) {
                 break;
             }
         }
-        if (arrive) {
-            System.out.println("1");
-        } else {
-            System.out.println("0");
-        }
 
+        if (isExist) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
     }
 
-    private static void dfs(int now, int depth) {
-//        System.out.println("now = " + now + " depth = " + depth);
-        if (depth == 5 || arrive) {
-            arrive = true;
+    private static void DFS(int now, int relationship) {
+        if (relationship == 4 || isExist) {
+            isExist = true;
             return;
         }
         visited[now] = true;
-        for (int i : A[now]) { //현재 노드의 연결 노드 중 방문하지 않은 노드 탐색
-            if (!visited[i]) {
-                dfs(i, depth + 1);
+
+        for (int next : A[now]) {
+            if (!visited[next]) {
+                DFS(next, relationship + 1);
             }
         }
         visited[now] = false; //노드가 연결되어 있을 수 있지만 true 상태이면 방문하지 않는다. 즉, 관계 파악할 수 없음
