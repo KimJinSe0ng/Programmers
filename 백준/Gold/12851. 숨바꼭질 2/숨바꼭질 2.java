52ec71995ecbfcb6466d1e9d87766b65d2983f1d
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-07-14 10:46:58 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 144 ms, Memory: 21348 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -7,17 +7,16 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
-    static boolean[] visited;
-    static int[] time;
-    static int minTime = Integer.MAX_VALUE; //제일 낮은 깊이
-    static int way = 0;
+    static int[] visited;
+    static int ways = 0;
+    static int min_time = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        visited = new boolean[100001]; //0~100,000
-        time = new int[100001];
+        visited = new int[100001];
 
         if (N >= K) {
             System.out.println(N - K);
@@ -25,52 +24,53 @@ public class Main {
             return;
         }
 
-        BFS(N);
-        System.out.println(time[K]);
-        System.out.println(way);
+        BFS();
+
+        System.out.println(visited[K]);
+        System.out.println(ways);
     }
 
-    private static void BFS(int pos) {
+    private static void BFS() {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(pos);
-        visited[pos] = true;
+        queue.add(N);
 
         while (!queue.isEmpty()) {
             int now = queue.poll();
-
-            if (minTime < time[now]) {
+            
+            if (visited[now] > min_time) {
                 return;
             }
+            
 
-            for (int d = 0; d < 3; d++) {
-                int nextPos = move(now, d);
+            for (int m = 0; m < 3; m++) {
+                int next = move(now, m);
 
-                if (nextPos < 0 || nextPos > 100000) { //범위 밖이면 pass
+                if (next < 0 || next > 100000) {
                     continue;
                 }
 
-                if (nextPos == K) {
-                    minTime = time[now]; //수빈이를 만난 최단시간
-                    way++;
+                if (next == K) {
+                    min_time = visited[now];
+                    ways++;
                 }
 
-                //방문하지 않았거나 한번의 이동으로 방문할 수 있다면 -> 이 위치를 최단 시간으로 방문할 수 있는 또 다른 경로를 발견한 경우
-                if (time[nextPos] == 0 || time[nextPos] == time[now] + 1) {
-                    queue.add(nextPos);
-                    time[nextPos] = time[now] + 1;
-//                    visited[nextPos] = true;
+                if (visited[next] == 0 || visited[next] == visited[now] + 1) {
+                    visited[next] = visited[now] + 1;
+                    queue.add(next);
                 }
             }
         }
     }
 
-    private static int move(int pos ,int index) {
-        if (index == 0) {
-            return pos - 1;
-        } else if (index == 1) {
-            return pos + 1;
-        } else {
-            return pos * 2;
+    private static int move(int pos, int index) {
+        switch (index) {
+            case 0:
+                return pos - 1;
+            case 1:
+                return pos + 1;
+            case 2:
+                return pos * 2;
         }
+        return pos;
     }
 }
