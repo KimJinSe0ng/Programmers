dcd8d59fdb989273171e67bdc0509056d5d4ad97
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-07-16 21:13:21 +0900

    [Silver I] Title: 회의실 배정, Time: 1048 ms, Memory: 173592 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,58 +1,35 @@
-import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Comparator;
- 
+import java.util.Scanner;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		
-		/*
-		  time[][0] 은 시작시점을 의미 
-		  time[][1] 은 종료시점을 의미 
-		*/
-		int[][] time = new int[N][2];
-		
-		
-		for(int i = 0; i < N; i++) {
-			time[i][0] = in.nextInt();	// 시작시간 
-			time[i][1] = in.nextInt();	// 종료시간 
-		}
-		
-		
-		// 끝나는 시간을 기준으로 정렬하기 위해 compare 재정의 
-		Arrays.sort(time, new Comparator<int[]>() {
-			
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				
-				// 종료시간이 같을 경우 시작시간이 빠른순으로 정렬해야한다.  
-				if(o1[1] == o2[1]) {
-					return o1[0] - o2[0];
-				}
-				
-				return o1[1] - o2[1];
-			}
- 
-		});
-		
-		int count = 0;
-		int prev_end_time = 0;
-		
-		for(int i = 0; i < N; i++) {
-			
-			// 직전 종료시간이 다음 회의 시작 시간보다 작거나 같다면 갱신 
-			if(prev_end_time <= time[i][0]) {
-				prev_end_time = time[i][1];
-				count++;
-			}
-		}
-		
-		System.out.println(count);
-	}
- 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] A = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            A[i][0] = sc.nextInt();
+            A[i][1] = sc.nextInt();
+        }
+
+        Arrays.sort(A, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] S, int[] E) {
+                if (S[1] == E[1]) { //종료 시간이 같을 때
+                    return S[0] - E[0];
+                }
+                return S[1] - E[1];
+            }
+        });
+
+        int count = 0;
+        int end = -1;
+        for (int i = 0; i < N; i++) { //겹치지 않는 다음 회의가 나온 경우
+            if (A[i][0] >= end) { //종료 시간 업데이트하기
+                end = A[i][1];
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
