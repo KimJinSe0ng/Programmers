366192417c9c9aa4ccc1aa373ce23d5bc6274cd5
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-07-26 13:18:38 +0900

    [Silver IV] Title: 청기 백기, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main { //제곱수를 구하는 문제
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println((int) Math.sqrt(N));
+    }
+}
