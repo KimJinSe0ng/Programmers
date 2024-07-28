f997d89a83badcbc84121d019d25f56b5c5dc930
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-07-28 15:58:53 +0900

    [Silver III] Title: 숫자 야구, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -7,7 +7,6 @@ public class Main {
     static int N;
     static int[][] hints;
     static int answer = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,37 +20,51 @@ public class Main {
             hints[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i < 10; i++) {
-            for (int j = 1; j < 10; j++) {
-                for (int k = 1; k < 10; k++) {
-                    if (i == j || j == k || k == i) {
+        for (int i = 1; i < 10; i++) { //1~9 : 100의자리
+            for (int j = 1; j < 10; j++) { //10의자리
+                for (int k = 1; k < 10; k++) { //1의자리
+                    if (i == j || j == k || k == i) { //숫자 중복은 패스
                         continue;
                     }
 
                     int count = 0;
-                    
+
                     for (int[] hint : hints) {
                         int number = hint[0];
                         int strike = hint[1];
                         int ball = hint[2];
 
-                        int guess1 = i, guess2 = j, guess3 = k;
-                        int num1 = number / 100;
-                        int num2 = (number / 10) % 10;
-                        int num3 = number % 10;
-
                         int strike_count = 0;
                         int ball_count = 0;
 
-                        if (guess1 == num1) strike_count++;
-                        if (guess2 == num2) strike_count++;
-                        if (guess3 == num3) strike_count++;
+                        int guess1 = i;
+                        int guess2 = j;
+                        int guess3 = k;
+                        int num1 = number / 100; //100의자리
+                        int num2 = (number / 10) % 10; //10의자리
+                        int num3 = number % 10; //1의자리
 
-                        if (guess1 == num2 || guess1 == num3) ball_count++;
-                        if (guess2 == num1 || guess2 == num3) ball_count++;
-                        if (guess3 == num1 || guess3 == num2) ball_count++;
+                        if (num1 == guess1) {
+                            strike_count++;
+                        }
+                        if (num2 == guess2) {
+                            strike_count++;
+                        }
+                        if (num3 == guess3) {
+                            strike_count++;
+                        }
 
-                        if (strike == strike_count && ball == ball_count) {
+                        if (num1 == guess2 || num1 == guess3) {
+                            ball_count++;
+                        }
+                        if (num2 == guess1 || num2 == guess3) {
+                            ball_count++;
+                        }
+                        if (num3 == guess1 || num3 == guess2) {
+                            ball_count++;
+                        }
+
+                        if (strike_count == strike && ball_count == ball) {
                             count++;
                         }
                     }
