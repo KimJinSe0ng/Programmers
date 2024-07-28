61fe21941ff9ef58fc9c46ddea03a32ccb47975f
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-07-28 21:41:56 +0900

    [Silver III] Title: N과 M (7), Time: 652 ms, Memory: 123604 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int[] A;
+    public static int[] nums;
+    public static int N, M;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        A = new int[M];
+        nums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        recur(0);
+        System.out.println(sb);
+
+    }
+
+    public static void recur(int depth) {
+        if (depth == M) {
+            for (int val : A) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            A[depth] = nums[i];
+            recur(depth + 1);
+        }
+    }
+}
