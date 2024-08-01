9e6778e7731616a5b35b8866a64247955d25f771
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-08-01 17:49:48 +0900

    [Gold V] Title: LCS, Time: 172 ms, Memory: 18688 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -2,42 +2,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
+    static String[] S1;
+    static String[] S2;
+    static int[][] dp;
 
-	static char[] str1;
-	static char[] str2;
-	static Integer[][] dp;
-	static int max = Integer.MIN_VALUE;
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		str1 = br.readLine().toCharArray();
-		str2 = br.readLine().toCharArray();
-		
-		dp = new Integer[str1.length][str2.length];
+        S1 = st.nextToken().split("");
 
+        st = new StringTokenizer(br.readLine());
+        S2 = st.nextToken().split("");
 
-		System.out.println(lcs(str1.length-1, str2.length-1));
-}
-	public static int lcs(int a, int b) {
-		
-		if(a<0||b<0)
-			return 0;
-		
-		if(dp[a][b]==null) {
-			dp[a][b]=0;
-			if(str1[a] == str2[b])
-				dp[a][b] = lcs(a-1, b-1)+1;
-			else
-				dp[a][b] = Math.max(lcs(a-1,b), lcs(a,b-1));
-		}
-		
-		return dp[a][b];
-	}
+        dp = new int[S1.length + 1][S2.length + 1];
+        for (int i = 1; i <= S1.length; i++) { //문자 하나 결정
+            for (int j = 1; j <= S2.length; j++) {
+                if (S1[i - 1].equals(S2[j - 1])) { //두 문자가 같으면
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else { //두 문자열이 다르면 둘 중 하나 지운 값중 큰 것을 가져오겠다.
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
 
+        System.out.println(dp[S1.length][S2.length]);
+    }
 }
