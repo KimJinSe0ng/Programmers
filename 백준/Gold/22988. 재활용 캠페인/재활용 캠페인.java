81c9ebbe2d86371b34ff81db8c77ea9272b320c9
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-08-02 00:11:51 +0900

    [Gold II] Title: 재활용 캠페인, Time: 484 ms, Memory: 37272 KB -BaekjoonHub

--- 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.java
+++ 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.java
@@ -5,30 +5,39 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static long X;
+    static long[] bottles;
+
+    /**
+     * Java에서는 배열의 크기나 인덱스는 int 타입으로 처리되기 때문에, long 타입으로 배열의 인덱스나 크기를 사용할 수는 없습니다.
+     * 다만, 배열의 요소가 long 타입인 것은 가능합니다.
+     * 코드에서 long 타입의 배열 요소를 사용하는 부분과 인덱스를 int 타입으로 처리하는 부분을 분리해야 합니다.
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        X = Long.parseLong(st.nextToken());
 
-        int N = Integer.parseInt(st.nextToken());
-        long X = Long.parseLong(st.nextToken());
-        long[] arr = new long[N];
+        bottles = new long[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
+            bottles[i] = Long.parseLong(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(bottles);
 
         int s = 0;
         int e = N - 1;
+        int count = 0;
         int remain = 0;
-        int cnt = 0;
 
         while (s <= e) {
-            if (arr[e] == X) {
-                cnt++;
-                e--;
+            if (bottles[e] == X) {
+                count++;
+                e -= 1;
                 continue;
             }
 
@@ -37,16 +46,16 @@ public class Main {
                 break;
             }
 
-            if (arr[e] + arr[s] >= X / 2.0) {
-                cnt++;
-                s++;
-                e--;
-            } else {
-                s++;
+            if (bottles[s] + bottles[e] >= X / 2.0) { //두 병 용량의 합이 총량의 절반에 가까우면, 남은 절반을 채워주기 때문에 꽉 채운 용기를 받을 수 있다.
+                count++;
+                e -= 1;
+                s += 1;
+            } else { //절반에 가깝지 않다면
+                s += 1;
                 remain++;
             }
         }
 
-        System.out.println(cnt + remain / 3);
+        System.out.println(count + (remain / 3));
     }
 }
