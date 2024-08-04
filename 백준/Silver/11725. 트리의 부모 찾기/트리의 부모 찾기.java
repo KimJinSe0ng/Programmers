dd46bd70d4d46fd965bfbacf4a7c2011590bdc71
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-08-04 17:54:57 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1104 ms, Memory: 83628 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,43 +1,49 @@
-import java.io.*;  
-import java.util.*;  
-  
-public class Main {  
-  
-    public static void main(String[] args) throws IOException {  
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
-        int n = Integer.parseInt(br.readLine());  
-        int[] parent = new int[n + 1];  
-        ArrayList<Integer>[] adj = new ArrayList[n + 1];  
-        for (int i = 1; i <= n; i++) {  
-            adj[i] = new ArrayList<>();  
-        }  
-        boolean[] visited = new boolean[n + 1];  
-        StringTokenizer st;  
-        for (int i = 1; i < n; i++) {  
-            st = new StringTokenizer(br.readLine());  
-            int a = Integer.parseInt(st.nextToken());  
-            int b = Integer.parseInt(st.nextToken());  
-            adj[a].add(b);  
-            adj[b].add(a);  
-        }  
-  
-        Queue<Integer> queue = new LinkedList<>();  
-        queue.add(1);  
-        visited[1] = true;  
-while (!queue.isEmpty()) {  
-            int cur = queue.poll();  
-            for (int next : adj[cur]) {  
-                if (visited[next]) {  
-                    continue;  
-                }  
-                visited[next] = true;  
-                queue.add(next);  
-                parent[next] = cur;  
-            }  
-        }  
-  
-        for (int i = 2; i <= n; i++) {  
-            System.out.println(parent[i]);  
-        }  
-    }  
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static ArrayList<Integer>[] tree;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        tree = new ArrayList[N + 1];
+        parent = new int[N + 1];
+
+        for (int i = 1; i < tree.length; i++) {
+            tree[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            tree[s].add(e);
+            tree[e].add(s);
+        }
+
+        dfs(1, 0);
+
+        for (int i = 2; i <= N; i++) {
+            System.out.println(parent[i]);
+        }
+    }
+
+    public static void dfs(int node, int prev) {
+        for (int next : tree[node]) {
+//            System.out.printf("node: %d -- next: %d, prev: %d\n", node, next, prev);
+            if (next == prev) {
+                continue;
+            }
+            parent[next] = node; 
+            dfs(next, node);
+        }
+    }
 }
