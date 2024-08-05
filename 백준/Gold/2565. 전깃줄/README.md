cf7573304815bcf0fbfb2d095ddc1e1333740b00
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-08-05 13:46:17 +0900

    [Gold V] Title: 전깃줄, Time: 108 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/README.md
+++ 백준/Gold/2565. 전깃줄/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 14560 KB, 시간: 140 ms
+메모리: 14244 KB, 시간: 108 ms
 
 ### 분류
 
@@ -12,7 +12,7 @@
 
 ### 제출 일자
 
-2024년 4월 15일 23:46:13
+2024년 8월 5일 13:46:06
 
 ### 문제 설명
 
@@ -20,7 +20,7 @@
 
 <p>예를 들어, < 그림 1 >과 같이 전깃줄이 연결되어 있는 경우 A의 1번 위치와 B의 8번 위치를 잇는 전깃줄, A의 3번 위치와 B의 9번 위치를 잇는 전깃줄, A의 4번 위치와 B의 1번 위치를 잇는 전깃줄을 없애면 남아있는 모든 전깃줄이 서로 교차하지 않게 된다.</p>
 
-<p style="text-align: center;"><img alt="" src="" style="width: 183px; height: 288px;"></p>
+<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/d90221dd-eb80-419f-bdfb-5dd4ebac23af/-/preview/" style="width: 183px; height: 288px;"></p>
 
 <p style="text-align: center;">< 그림 1 ></p>
 
--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -1,32 +1,47 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
-
+    static int N;
+    static int[][] pole;
+    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = Integer.parseInt(st.nextToken());
+        pole = new int[N][2];
+        dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            pole[i][0] = a;
+            pole[i][1] = b;
         }
 
-        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+        //왼쪽 전봇대에서 번호 순으로 정렬
+        Arrays.sort(pole, Comparator.comparingInt(a -> a[0]));
 
-        int[] dp = new int[n];
         int max = 0;
 
-        for (int i = 0; i < n; i++) {
-            dp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (arr[i][1] > arr[j][1]) {
+        //최장 증가 부분수열 찾기
+        for (int i = 0; i < N; i++) { //왼쪽 전봇대 번호 선택
+            for (int j = 0; j < i; j++) { //오른쪽 전봇대는 왼쪽 전봇대 번호만큼까지만 확인
+                if (pole[i][1] > pole[j][1]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
             max = Math.max(max, dp[i]);
         }
 
-        System.out.println(n - max);
+        System.out.println(N - max);
+
     }
 }
