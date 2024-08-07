a5e7c3143e92d90b64c6a11c600580e67e2b91a7
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-08-07 15:25:11 +0900

    [Platinum IV] Title: 체커, Time: 160 ms, Memory: 21356 KB -BaekjoonHub

--- 백준/Platinum/1090. 체커/체커.java
+++ 백준/Platinum/1090. 체커/체커.java
@@ -1,16 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class Main { //메모리 초과
+public class Main {
     static int N;
     static int[][] checkers;
+//    static int min_x = Integer.MAX_VALUE;
+//    static int max_x = Integer.MIN_VALUE;
+//    static int min_y = Integer.MAX_VALUE;
+//    static int max_y = Integer.MIN_VALUE;
     static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         checkers = new int[N][2];
         answer = new int[N];
@@ -25,20 +31,26 @@ public class Main { //메모리 초과
             checkers[i][1] = Integer.parseInt(st.nextToken());
             arr_x[i] = checkers[i][0];
             arr_y[i] = checkers[i][1];
+//            min_x = Math.min(x, min_x);
+//            min_y = Math.min(y, min_y);
+//            max_x = Math.max(x, max_x);
+//            max_y = Math.max(y, max_y);
         }
 
-        int[] tmp = new int[N];
-        for (int x : arr_x) { //모든 좌표 탐색
+        int[] temp = new int[N];
+
+        for (int x : arr_x)
+        {
             for (int y : arr_y) {
                 for (int i = 0; i < checkers.length; i++) {
-                    tmp[i] = Math.abs(x - checkers[i][0]) + Math.abs(y - checkers[i][1]); //거리 비용
+                    temp[i] = Math.abs(x - checkers[i][0]) + Math.abs(y - checkers[i][1]);
                 }
 
-                Arrays.sort(tmp);
-
+                Arrays.sort(temp); //짧은 거리 순으로 정렬되어 있음
+                
                 int sum = 0;
-                for (int i = 0; i < tmp.length; i++) {
-                    sum += tmp[i];
+                for (int i = 0; i < temp.length; i++) {
+                    sum += temp[i];
                     if (answer[i] == -1) {
                         answer[i] = sum;
                     } else {
@@ -54,5 +66,4 @@ public class Main { //메모리 초과
 
         System.out.println(sb.toString().trim());
     }
-
 }
