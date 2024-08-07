82c957ee6a5dddb59816c5f94b3251e6978cc7d6
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-08-07 12:18:41 +0900

    [Bronze I] Title: 암호 키, Time: 184 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/1816. 암호 키/암호 키.java
+++ 백준/Bronze/1816. 암호 키/암호 키.java
@@ -1,24 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    static int N;
 
-        for (int i = 0; i < n; i++) {
-            long num = sc.nextLong();
-            boolean isTrue = true;
-
-            for (int j = 2; j <= 1_000_000; j++) {
-                if (num % j == 0) {
-                    System.out.println("NO");
-                    isTrue = false;
-                    break;
-                }
-            }
-            if (isTrue) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            long S = Long.parseLong(st.nextToken());
+            if (isPrime(S)) {
                 System.out.println("YES");
+            } else {
+                System.out.println("NO");
             }
         }
+    }
+
+    private static boolean isPrime(long S) {
+        for (int i = 2; i <= 1000000; i++) {
+            if (S % i == 0) { //나눠지면 소수이고, 10^6보다 작으면 NO
+                return false;
+            }
+        }
+        return true;
     }
 }
