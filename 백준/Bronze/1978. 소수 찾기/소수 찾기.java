c2c60f216361e387ad5914852870eefde605448f
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-08-07 16:21:16 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,16 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int count = 0;
+    static int N;
+    static int[] nums;
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        nums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            int num = sc.nextInt();
-            if (isPrime(num)) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (isPrime(nums[i])) {
                 count++;
             }
         }
+
         System.out.println(count);
     }
 
@@ -18,7 +32,7 @@ public class Main {
         if (num == 1) {
             return false;
         }
-        for (int i = 2; i <= Math.sqrt(num); i++) {
+        for (int i = 2; i < num; i++) {
             if (num % i == 0) {
                 return false;
             }
