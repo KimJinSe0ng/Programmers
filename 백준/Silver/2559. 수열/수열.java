ad3eead094f9893df3d0348cb878ca9a4046ca48
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-08-08 13:01:11 +0900

    [Silver III] Title: 수열, Time: 236 ms, Memory: 24048 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,23 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int N, K;
+    static int[] A;
+    static int[] prefix;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n + 1];
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        A = new int[N];
+        prefix = new int[N + 1];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        int answer = Integer.MIN_VALUE;
-        for (int i = k; i <= n; i++) {
-            answer = Math.max(answer, arr[i] - arr[i - k]);
+
+        // prefix 배열 초기화
+        for (int i = 0; i < N; i++) {
+            prefix[i + 1] = prefix[i] + A[i];
         }
-        System.out.println(answer);
+
+        // 부분 합 계산
+        int maxSum = Integer.MIN_VALUE;
+        for (int j = K; j <= N; j++) {
+            int currentSum = prefix[j] - prefix[j - K];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        // 결과 출력
+        System.out.println(maxSum);
+
     }
 }
