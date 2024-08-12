a1f4d799d9adacae6d0f71506778c3a9530cc270
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-08-12 13:06:53 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 120 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -5,19 +5,19 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[][] ingredient;
+    static int[][] items;
     static int answer = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        ingredient = new int[N][2];
+        items = new int[N][2];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            ingredient[i][0] = Integer.parseInt(st.nextToken());
-            ingredient[i][1] = Integer.parseInt(st.nextToken());
+            items[i][0] = Integer.parseInt(st.nextToken());
+            items[i][1] = Integer.parseInt(st.nextToken());
         }
 
         recur(0, 1, 0, 0);
@@ -25,18 +25,18 @@ public class Main {
     }
 
     private static void recur(int idx, int sour, int bitter, int use) {
-        if (idx == N) { //음식을 모두 사용했다면
-            if (use == 0) { //아무 재료도 사용하지 않았다면 그냥 종료
+        if (idx == N) { //재료를 다 썼으면 종료
+            if (use == 0) { //재료를 하나도 사용하지 않은 경우 종료
                 return;
             }
-            int result = Math.abs(sour - bitter);
-            answer = Math.min(answer, result);
+            int diff = Math.abs(sour - bitter);
+            answer = Math.min(diff, answer);
             return;
         }
-        //재귀 함수는 모든 경우의 수를 구현해야 함
+
         //재료를 사용한 경우
-        recur(idx + 1, sour * ingredient[idx][0], bitter + ingredient[idx][1], use + 1);
-        //재료를 사용하지 않은 경우
+        recur(idx + 1, sour * items[idx][0], bitter + items[idx][1], use + 1);
+        //재료를 쓰지 않은 경우 : 다음 재료 선택
         recur(idx + 1, sour, bitter, use);
     }
 }
