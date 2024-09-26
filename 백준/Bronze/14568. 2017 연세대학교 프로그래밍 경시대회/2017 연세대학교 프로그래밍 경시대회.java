12567857424fe4ac0b0c4de3420081aa4da47319
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-09-26 23:43:45 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -1,24 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     static int N;
     static int answer = 0;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
 
-        for (int t = 0; t <= N; t++) { //택희
-            for (int y = 0; y <= N; y++) { //영훈
-                for (int n = 0; n <= N; n++) { //남규
-                    if (t + y + n == N) {
-                        if (n >= y + 2) {
-                            if (t != 0 && y != 0 && n != 0) {
-                                if (t % 2 != 1) {
+        for (int A = 0; A <= N; A++) { //택희
+            for (int B = 0; B <= N; B++) { //영훈
+                for (int C = 0; C <= N; C++) { //남규
+                    if (A + B + C == N) { //남는 사탕 없어야 함
+                        if (C >= B + 2) { //남규는 영훈이보다 2개 이상
+                            if (A != 0 && B != 0 && C != 0) { //셋 중 사탕을 0개 받는 사람 없어야 함
+                                if (A % 2 != 1) { //택희의 사탕 수는 짝수여야 함
                                     answer++;
                                 }
                             }
@@ -27,7 +25,6 @@ public class Main {
                 }
             }
         }
-
         System.out.println(answer);
     }
 }
