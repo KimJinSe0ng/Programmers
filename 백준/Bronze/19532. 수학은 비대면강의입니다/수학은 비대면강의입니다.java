3cf9b245cdea8f27ecad6d8f32723a550fcc7a2d
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-09-27 15:55:56 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 140 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,35 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static int a, b, c, d, e, f;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        int f = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        d = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        f = Integer.parseInt(st.nextToken());
 
-        int x = 0;
-        int y = 0;
-
-        for(int i = -999; i < 1000; i++){
-            for(int j = -999; j < 1000; j++){
-                if((a*i+b*j==c) && (d*i+e*j==f)){
-                    x = i;
-                    y = j;
-                    break;
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if (a * x + b * y == c) {
+                    if (d * x + e * y == f) {
+                        System.out.printf("%d %d\n", x, y);
+                    }
                 }
             }
         }
-
-        System.out.println(x + " " + y);
-
-        br.close();
     }
 }
