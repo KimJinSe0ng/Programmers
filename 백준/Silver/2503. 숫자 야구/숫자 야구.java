9ec7f5df3900e04df94fe6a8d54a2b9108c1541a
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-09-27 15:45:23 +0900

    [Silver III] Title: 숫자 야구, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -5,78 +5,74 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[][] hints;
+    static int[][] question;
     static int answer = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        hints = new int[N][3];
+        question = new int[N][3];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            hints[i][0] = Integer.parseInt(st.nextToken());
-            hints[i][1] = Integer.parseInt(st.nextToken());
-            hints[i][2] = Integer.parseInt(st.nextToken());
+            question[i][0] = Integer.parseInt(st.nextToken());
+            question[i][1] = Integer.parseInt(st.nextToken());
+            question[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        recur(0, 100);
+        for (int num100 = 1; num100 <= 9; num100++) {
+            for (int num10 = 1; num10 <= 9; num10++) {
+                for (int num1 = 1; num1 <= 9; num1++) {
+                    if (num1 == num10 || num10 == num100 || num100 == num1) {
+                        continue;
+                    }
+
+                    int passedHintCount = 0;
+
+                    for (int i = 0; i < N; i++) {
+                        int guess = question[i][0];
+                        int strike = question[i][1];
+                        int ball = question[i][2];
+
+                        int guess100 = guess / 100;
+                        int guess10 = (guess % 100) / 10;
+                        int guess1 = guess % 10;
+
+                        int strike_count = 0;
+                        int ball_count = 0;
+
+                        if (num100 == guess100) {
+                            strike_count++;
+                        }
+                        if (num10 == guess10) {
+                            strike_count++;
+                        }
+                        if (num1 == guess1) {
+                            strike_count++;
+                        }
+
+                        if (num100 == guess10 || num100 == guess1) {
+                            ball_count++;
+                        }
+                        if (num10 == guess100 || num10 == guess1) {
+                            ball_count++;
+                        }
+                        if (num1 == guess100 || num1 == guess10) {
+                            ball_count++;
+                        }
+
+                        if (strike_count == strike && ball_count == ball) {
+                            passedHintCount++;
+                        }
+                    }
+
+                    if (passedHintCount == N) {
+                        answer++;
+                    }
+                }
+            }
+        }
         System.out.println(answer);
-    }
-
-    private static boolean checker(int idx, int number) {
-        int _number = hints[idx][0];
-        int _strike = hints[idx][1];
-        int _ball = hints[idx][2];
-
-        int strike = 0;
-        int ball = 0;
-
-        int _A = _number / 100;
-        int _B = (_number % 100) / 10;
-        int _C = _number % 10;
-
-        int A = number / 100;
-        int B = (number % 100) / 10;
-        int C = number % 10;
-
-        if (A == 0 || B == 0 || C == 0) {
-            return false;
-        }
-
-        if (A == B || A == C || B == C) {
-            return false;
-        }
-
-        if (A == _A) strike++;
-        if (B == _B) strike++;
-        if (C == _C) strike++;
-
-        if (A == _B || A == _C) ball++;
-        if (B == _A || B == _C) ball++;
-        if (C == _A || C == _B) ball++;
-
-        return strike == _strike && ball == _ball;
-
-    }
-
-    private static void recur(int idx, int num) {
-        if (idx == N) {
-            answer++;
-            recur(0, num + 1); //정답의 개수가 1개 이상일 수 있으니 계속 탐색
-            return;
-        }
-
-        if (num == 1000) {
-            return;
-        }
-
-        if (checker(idx, num)) { //힌트를 통과했다면
-            recur(idx + 1, num);
-        } else {
-            recur(0, num + 1);
-        }
     }
 }
