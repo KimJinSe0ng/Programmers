2a842349d9819e16b438d724765c8fd11d24266c
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-09-28 13:38:43 +0900

    [level 0] Title: 리스트 자르기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,29 +1,30 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        List<Integer> list = new ArrayList<>();
         int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
-        if(n == 1) {
-            for(int i =0; i<b+1; i++) {
-                list.add(num_list[i]);
-            }
-        }else if(n == 2) {
-            for(int i = a; i<num_list.length; i++) {
-                list.add(num_list[i]);
-            }
-        }else if(n ==3) {
-            for(int i = a; i<b+1; i++) {
-                list.add(num_list[i]);
-            }
-        }else if(n == 4) {
-            for(int i = a; i<b+1; i+=c) {
-                list.add(num_list[i]);
+        if (n == 1) {
+            // 0번 인덱스부터 b번 인덱스까지 슬라이싱
+            return Arrays.copyOfRange(num_list, 0, b + 1);
+        } else if (n == 2) {
+            // a번 인덱스부터 마지막 인덱스까지 슬라이싱
+            return Arrays.copyOfRange(num_list, a, num_list.length);
+        } else if (n == 3) {
+            // a번 인덱스부터 b번 인덱스까지 슬라이싱
+            return Arrays.copyOfRange(num_list, a, b + 1);
+        } else if (n == 4) {
+            // a번 인덱스부터 b번 인덱스까지 c 간격으로 슬라이싱
+            int size = (b - a) / c + 1;  // 결과 리스트의 크기를 계산
+            int[] result = new int[size];
+            int idx = 0;
 
+            for (int i = a; i <= b; i += c) {
+                result[idx++] = num_list[i];
             }
+            return result;
         }
-        int[] answer = list.stream().mapToInt(x -> x).toArray();
-        return answer;
+        return new int[0];
     }
 }
