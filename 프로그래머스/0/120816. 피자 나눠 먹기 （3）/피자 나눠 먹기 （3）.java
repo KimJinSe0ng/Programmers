c9c430397f0f7e532da2fce342b380f775500123
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-09-28 15:07:09 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,10 +1,5 @@
 class Solution {
     public int solution(int slice, int n) {
-        int pizza = n / slice;
-        if(n % slice == 0) {
-            return pizza;
-        } else {
-            return pizza + 1;
-        }
+        return (n + (slice - 1)) / slice;
     }
 }
