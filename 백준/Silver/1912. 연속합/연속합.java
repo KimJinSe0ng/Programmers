557696c6b266722986035989e66b3aa8d92f8e29
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-09-29 22:46:30 +0900

    [Silver II] Title: 연속합, Time: 236 ms, Memory: 24568 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -5,30 +5,33 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[] A;
+    static int n;
+    static int[] numbers;
     static int[] prefix;
-    static int max_value = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        A = new int[N];
-        prefix = new int[N + 1];
+        n = Integer.parseInt(st.nextToken());
+        numbers = new int[n];
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        prefix[0] = A[0];
-        int max = prefix[0];
+        prefix = new int[n + 1];
+        
+        Arrays.fill(prefix, -1001);
 
-        for (int i = 1; i < N; i++) {
-            prefix[i] = Math.max(prefix[i - 1] + A[i], A[i]);
-            max = Math.max(max, prefix[i]);
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = Math.max(prefix[i] + numbers[i], numbers[i]);
         }
 
-        System.out.println(max);
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            maxSum = Math.max(maxSum, prefix[i]);
+        }
+
+        System.out.println(maxSum);
     }
 }
