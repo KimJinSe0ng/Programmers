e5ba72cf1e48e541788d35d28c1d994963981383
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-08 14:19:04 +0900

    [level 0] Title: 문자열 뒤집기, Time: 17.87 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(String my_string, int s, int e) {
-        String answer = my_string.substring(s,e+1);
-        
-        StringBuffer sb = new StringBuffer(answer);
-        String reversedStr = sb.reverse().toString();
-        
-        my_string = my_string.replaceAll(answer, reversedStr);
-        return my_string;
+        String before = my_string.substring(0, s);
+        String middle = new StringBuilder(my_string.substring(s, e + 1)).reverse().toString();
+        String after = my_string.substring(e + 1);
+
+        return before + middle + after;
     }
 }
