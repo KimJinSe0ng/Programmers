d8c5122494671b1af4a80a0a806be366439f6f50
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-08 09:21:08 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        //He110W0r1d
+        //0123456789
+        return my_string.substring(my_string.length() - n);
+    }
+}
