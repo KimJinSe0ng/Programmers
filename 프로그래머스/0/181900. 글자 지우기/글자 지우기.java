9b57af73ea43662b20505ce9494aed48e77fd12a
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-08 18:19:19 +0900

    [level 0] Title: 글자 지우기, Time: 3.69 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -2,17 +2,14 @@ import java.util.*;
 
 class Solution {
     public String solution(String my_string, int[] indices) {
-        String answer = "";
+        Arrays.sort(indices);
         
-        String[] tmp = my_string.split("");
+        StringBuilder sb = new StringBuilder(my_string);
         
-        for (int i = 0; i < indices.length; i++) {
-            tmp[indices[i]] = "";
+        for (int i = indices.length - 1; i >= 0; i--) {
+            sb.deleteCharAt(indices[i]);
         }
-
-        for (String x : tmp) {
-            answer += x;
-        }
-        return answer;
+        
+        return sb.toString();
     }
 }
