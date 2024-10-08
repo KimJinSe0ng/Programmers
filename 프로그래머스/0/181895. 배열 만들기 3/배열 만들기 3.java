879a37549af0bb15f91e6e61f77cd4c54e70cb7e
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-08 18:39:45 +0900

    [level 0] Title: 배열 만들기 3, Time: 10.45 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,20 +1,15 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        ArrayList<Integer> list = new ArrayList<>();
-
+        List<Integer> result = new ArrayList<>();
         for(int i = 0; i < intervals.length; i++) {
-            for(int j = intervals[i][0]; j <= intervals[i][1]; j++) {
-                list.add(arr[j]);
+            int s = intervals[i][0];
+            int e = intervals[i][1];
+            for(int j = s; j <= e; j++) {
+                result.add(arr[j]);
             }
         }
-
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-
-        return answer;
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
