cca49debf3ff52b54b9258ad9b0db057b73c8d55
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-08 19:39:08 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,15 +1,19 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        String[] answer = {};
-        for (int i = 0; i < str_list.length;i++) {
+        // "l" 또는 "r"이 나오는 첫 번째 위치를 찾기
+        for (int i = 0; i < str_list.length; i++) {
             if (str_list[i].equals("l")) {
+                // "l"이 나오면 왼쪽 부분 반환 (0부터 i-1까지)
                 return Arrays.copyOfRange(str_list, 0, i);
             } else if (str_list[i].equals("r")) {
+                // "r"이 나오면 오른쪽 부분 반환 (i+1부터 끝까지)
                 return Arrays.copyOfRange(str_list, i + 1, str_list.length);
             }
         }
-        return answer;
+        
+        // "l" 또는 "r"이 없으면 빈 배열 반환
+        return new String[]{};
     }
 }
