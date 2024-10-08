0a3c3b787c272880c94c8cefdcf1160e74dabcf8
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-08 19:44:57 +0900

    [level 0] Title: n개 간격의 원소들, Time: 2.89 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -2,15 +2,10 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        ArrayList<Integer> A = new ArrayList<>();
-        
-        int i = 0;
-        
-        while(i < num_list.length) {
-            A.add(num_list[i]);
-            i += n;
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < num_list.length; i += n) {
+            list.add(num_list[i]);
         }
-        
-        return A.stream().mapToInt(Integer::intValue).toArray();
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
