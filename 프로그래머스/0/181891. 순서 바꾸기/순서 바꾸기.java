92ef301bbcb8eeb28101af9295edcad0e7b59d95
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-08 19:26:54 +0900

    [level 0] Title: 순서 바꾸기, Time: 3.43 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,16 +1,14 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length];
-        int index = 0;
-
-        for (int i = n; i < num_list.length; i++) {
-            answer[index++] = num_list[i];
+        List<Integer> list = new ArrayList<>();
+        for(int i = n; i < num_list.length; i++) {
+            list.add(num_list[i]);
         }
-
-        for (int i = 0; i < n; i++) {
-            answer[index++] = num_list[i];
+        for(int i = 0 ; i < n; i++) {
+            list.add(num_list[i]);
         }
-
-        return answer;
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
