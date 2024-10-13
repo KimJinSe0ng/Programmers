a4810aa74db7bc141dee7c20f2519f5e7a767418
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-14 00:36:18 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 54.06 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,22 +1,23 @@
 class Solution {
     public int solution(int[] arr) {
-        int answer = 0;
         int count = 0;
-        int[] beforArr = new int[arr.length];
-        while(count != arr.length) {
-            count = 0;
-            beforArr = arr.clone();
-            for(int i =0; i<arr.length; i++) {
-            if(arr[i]%2 == 0 && arr[i] >= 50) {
-                arr[i] /= 2;
-            }else if(arr[i]%2 != 0 && arr[i] < 50) {
-                arr[i] = arr[i]*2 +1;
+        for(int j = 0; j < arr.length; j++) {
+            for(int i = 0 ; i < arr.length; i++) {
+                if(arr[i] >= 50 && arr[i] % 2 == 0) {
+                    arr[i] /= 2;
+                } else if(arr[i] < 50 && arr[i] % 2 == 1){
+                    arr[i] = arr[i] * 2 + 1;
+                } else {
+                    count++;
+                }
             }
-            if(arr[i] == beforArr[i] ) count ++;
-        }
-            answer ++;
+            if(count == arr.length) {
+                return j;
+            } else {
+                count = 0;
+            }
         }
         
-        return answer-1;
+        return arr.length;
     }
 }
