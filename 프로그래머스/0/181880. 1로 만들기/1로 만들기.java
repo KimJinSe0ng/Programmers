24db8af5b17bb57a77adc9be7593e727e574d93b
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-14 00:41:22 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,18 +1,22 @@
 class Solution {
     public int solution(int[] num_list) {
         int answer = 0;
-        
-        for (int num : num_list) {
-            while (num != 1) {
-                if (num % 2 == 0) {
-                    num /= 2;
-                } else {
-                    num = (num - 1) / 2;
-                }
-                answer++;
-            }
+        for(int i = 0; i < num_list.length; i++) {
+            answer += calculate(num_list[i]);
         }
-        
         return answer;
+    }
+    
+    private int calculate(int num) {
+        int cnt = 0;
+        while(num > 1) {
+            if(num % 2 == 0) {
+                num /= 2;
+            } else {
+                num = (num - 1) / 2;
+            }
+            cnt++;
+        }
+        return cnt;
     }
 }
--- 프로그래머스/0/181880. 1로 만들기/README.md
+++ 프로그래머스/0/181880. 1로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 1로 만들기 - 181880 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181880?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181880) 
 
 ### 성능 요약
 
-메모리: 72.7 MB, 시간: 0.03 ms
+메모리: 72.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 07일 23:17:15
+2024년 10월 14일 00:41:20
 
 ### 문제 설명
 
