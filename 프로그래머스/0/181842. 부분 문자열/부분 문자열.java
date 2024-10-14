909071a6d040fc2b212b43a6e1acf5b470a5d442
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-14 15:50:56 +0900

    [level 0] Title: 부분 문자열, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
-        
-        if (str2.contains(str1)) {
-            answer = 1;
+        if(str2.contains(str1)) {
+            return 1;
         } else {
-            answer = 0;
+            return 0;
         }
-        
-        return answer;
     }
 }
