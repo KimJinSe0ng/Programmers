550cbdf12087f62e1b8f422cfcd2e4baa78a90cc
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-14 17:46:45 +0900

    [Bronze V] Title: N 찍기, Time: 732 ms, Memory: 30652 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int T;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        T = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= T; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
