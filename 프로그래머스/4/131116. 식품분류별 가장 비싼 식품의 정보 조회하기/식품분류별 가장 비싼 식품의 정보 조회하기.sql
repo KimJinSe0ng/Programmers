31cf6066bd9c6ec01dcb3fd93601a215508c8779
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-17 16:01:35 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,23 +1,24 @@
-# SELECT
-#     CATEGORY,
-#     PRICE AS 'MAX_PRICE',
-#     PRODUCT_NAME
-# FROM
-#     FOOD_PRODUCT
-# WHERE PRICE IN (SELECT MAX(PRICE)
-#                 FROM FOOD_PRODUCT
-#                 GROUP BY CATEGORY) AND
-#       CATEGORY IN ('과자', '국', '김치', '식용유')
-# ORDER BY MAX_PRICE DESC;
+WITH MAX_PRICE_INFO AS (
+    SELECT
+        CATEGORY,
+        MAX(PRICE) AS MAX_PRICE
+    FROM
+        FOOD_PRODUCT
+    WHERE
+        CATEGORY IN ('과자', '국', '김치', '식용유')
+    GROUP BY
+        CATEGORY
+)
 
 SELECT
-    CATEGORY,
-    PRICE AS 'MAX_PRICE',
-    PRODUCT_NAME
+    FP.CATEGORY,
+    FP.PRICE,
+    FP.PRODUCT_NAME
 FROM
-    FOOD_PRODUCT
-WHERE (CATEGORY, PRICE) IN (SELECT CATEGORY, MAX(PRICE)
-                            FROM FOOD_PRODUCT
-                            GROUP BY CATEGORY
-                            HAVING CATEGORY IN ('과자', '국', '김치', '식용유'))
-ORDER BY MAX_PRICE DESC;
+    FOOD_PRODUCT FP
+JOIN
+    MAX_PRICE_INFO MPI ON FP.CATEGORY = MPI.CATEGORY
+AND
+    FP.PRICE = MPI.MAX_PRICE
+ORDER BY
+    FP.PRICE DESC;
