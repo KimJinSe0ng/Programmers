040419fa43b55cad6b625a33d1392d4dd7b69835
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-17 11:29:16 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.20 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        String[] str = s.split(" ");
+        int sum = 0;
+        for(int i = 0; i < str.length; i++) {
+            if(str[i].equals("Z")) {
+                sum -= Integer.parseInt(str[i - 1]);
+                continue;
+            }
+            sum += Integer.parseInt(str[i]);
+        }
+        return sum;
+    }
+}
