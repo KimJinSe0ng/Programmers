1c2c1a7effc70a0b51dbab5db5c1f07587507715
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-19 01:34:53 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -2,28 +2,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    static List<List<Integer>> tree = new ArrayList<>();
+    static ArrayList<Integer>[] tree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i <= n; i++) {
-            tree.add(new ArrayList<>());
+        tree = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            tree[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < n; i++) {
             String[] input = br.readLine().split(" ");
             int a = input[0].charAt(0) - 'A' + 1;
-            int b = input[1].charAt(0) - 'A' + 1;
-            int c = input[2].charAt(0) - 'A' + 1;
+            int b = input[1].charAt(0) == '.' ? -18 : input[1].charAt(0) - 'A' + 1;
+            int c = input[2].charAt(0) == '.' ? -18 : input[2].charAt(0) - 'A' + 1;
 
-            tree.get(a).add(b);
-            tree.get(a).add(c);
+            tree[a].add(b);  // 왼쪽 자식
+            tree[a].add(c);  // 오른쪽 자식
         }
 
         preOrder(1);
@@ -36,27 +37,27 @@ public class Main {
     // 전위 순회
     public static void preOrder(int start) {
         if (start != -18) {
-            System.out.print((char)(start + 'A' - 1));
-            preOrder(tree.get(start).get(0));
-            preOrder(tree.get(start).get(1));
+            System.out.print((char) (start + 'A' - 1));
+            preOrder(tree[start].get(0)); // 왼쪽
+            preOrder(tree[start].get(1)); // 오른쪽
         }
     }
 
     // 중위 순회
     public static void inOrder(int start) {
         if (start != -18) {
-            inOrder(tree.get(start).get(0));
-            System.out.print((char)(start + 'A' - 1));
-            inOrder(tree.get(start).get(1));
+            inOrder(tree[start].get(0)); // 왼쪽
+            System.out.print((char) (start + 'A' - 1));
+            inOrder(tree[start].get(1)); // 오른쪽
         }
     }
 
     // 후위 순회
     public static void postOrder(int start) {
         if (start != -18) {
-            postOrder(tree.get(start).get(0));
-            postOrder(tree.get(start).get(1));
-            System.out.print((char)(start + 'A' - 1));
+            postOrder(tree[start].get(0)); // 왼쪽
+            postOrder(tree[start].get(1)); // 오른쪽
+            System.out.print((char) (start + 'A' - 1));
         }
     }
 }
