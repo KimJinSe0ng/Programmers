ba0e567d9214e2faa567c94f0b98da5db6d3a10b
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-10-26 23:17:28 +0900

    [level 1] Title: K번째수, Time: 5.23 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,17 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        for(int i = 0; i < answer.length; i++) {
-            int[] command = commands[i];
-            int from = command[0] - 1;
-            int to = command[1];
-            int k = command[2] - 1;
-            
-            int[] sub = Arrays.copyOfRange(array, from, to);
-            Arrays.sort(sub);
-            answer[i] = sub[k];
+        List<Integer> answer = new ArrayList<>();
+        for(int[] command : commands) {
+            int start = command[0];
+            int end = command[1];
+            int times = command[2];
+            int[] copy = new int[end - start + 1];
+            System.arraycopy(array, start - 1, copy, 0, end - start + 1);
+            Arrays.sort(copy);
+            answer.add(copy[times - 1]);
         }
-        return answer;
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
