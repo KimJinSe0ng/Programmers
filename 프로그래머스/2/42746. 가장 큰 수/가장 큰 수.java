3dd55689bc5cf5e24f9766e66c786c767a5dc643
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-01 14:37:06 +0900

    [level 2] Title: 가장 큰 수, Time: 313.60 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,16 +1,18 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        return Arrays.stream(numbers)
-                .mapToObj(String::valueOf)
-                .sorted((s1, s2) -> {
-                    int original = Integer.parseInt(s1 + s2);
-                    int reversed = Integer.parseInt(s2 + s1);
-                    return reversed - original;
-                })
-                .collect(Collectors.joining("")) //단순히 문자열로 구성된 Stream을 하나의 문자열로 구성하는 것은 joining()을 사용하면 된다.
-                .replaceAll("^0+", "0"); //^는 문자열의 시작, +는 1회 이상 반복 -> 문자열의 시작 부분에서 0이 반복해서 나타난다면 0 하나로 대체하라
+        String[] str = new String[numbers.length];
+        for(int i = 0; i < str.length; i++) {
+            str[i] = Integer.toString(numbers[i]);
+        }
+        Arrays.sort(str, (a, b) -> (b + a).compareTo(a + b));
+        String result = String.join("", str);
+
+        if (result.startsWith("0")) {
+            return "0";
+        }
+        
+        return result;
     }
 }
