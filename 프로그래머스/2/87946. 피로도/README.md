eb34a24804215ee15ca2677cc1f29261b6110094
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-02 00:48:59 +0900

    [level 2] Title: 피로도, Time: 2.97 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 72.2 MB, 시간: 0.05 ms
+메모리: 75.7 MB, 시간: 2.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 05일 23:44:40
+2024년 11월 02일 00:48:58
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,23 @@
-class Solution {  
-    static boolean[] visited;  
-    static int count = 0;  
-  
-    public int solution(int k, int[][] dungeons) {  
-        visited = new boolean[dungeons.length];  
-        dfs(0, k, dungeons);  
-        return count;  
-    }  
-      
-    private void dfs(int depth, int fatigue, int[][] dungeons){  
-        for (int i = 0; i < dungeons.length; i++){  
-            if (visited[i] || dungeons[i][0] > fatigue) {  
-                continue;  
-            }  
-            visited[i] = true;  
-            dfs(depth + 1, fatigue - dungeons[i][1], dungeons);  
-            visited[i] = false;  
-        }  
-        count = Math.max(count, depth);  
-    }  
+class Solution {
+    static int answer = 0;
+
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        recur(k, dungeons, visited, 0);
+        return answer;
+    }
+    
+    private void recur(int remain, int[][] dungeons, boolean[] visited, int ans) {
+        answer = Math.max(answer, ans);
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i] || remain < dungeons[i][0]) {
+                continue;
+            }
+            
+            visited[i] = true;
+            recur(remain - dungeons[i][1], dungeons, visited, ans + 1);
+            visited[i] = false;
+        }
+    }
 }
