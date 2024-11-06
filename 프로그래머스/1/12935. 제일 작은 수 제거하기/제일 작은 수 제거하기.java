dadaf0ba22fe619189b8451f1eded98363175667
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-06 14:53:27 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.60 ms, Memory: 95.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.length == 1) {
+            return new int[] {-1};
+        }
+
+        int min = arr[0];
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+        }
+
+        int[] result = new int[arr.length - 1];
+        int index = 0;
+        for (int num : arr) {
+            if (num != min) {
+                result[index++] = num;
+            }
+        }
+
+        return result;
+    }
+}
