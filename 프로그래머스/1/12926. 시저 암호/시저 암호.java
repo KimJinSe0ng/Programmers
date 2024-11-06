8879b5b1d4c2da021ad78486c2cfa1336df04a52
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-06 19:51:17 +0900

    [level 1] Title: 시저 암호, Time: 0.77 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,20 +1,17 @@
 class Solution {
     public String solution(String s, int n) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
+
         for (char c : s.toCharArray()) {
-            sb.append(push(c, n));
+            if (c == ' ') {
+                result.append(c);
+            } else if (c >= 'A' && c <= 'Z') {
+                result.append((char) ((c - 'A' + n) % 26 + 'A'));
+            } else if (c >= 'a' && c <= 'z') {
+                result.append((char) ((c - 'a' + n) % 26 + 'a'));
+            }
         }
-        return sb.toString();
-    }
-    
-    private char push(char c, int n) {
-        //알파벳이 아닌 경우 문자를 그대로 이어 붙이기
-        if (!Character.isAlphabetic(c)) return c;
-        
-        //알파벳인 경우 n만큼 밀어 이어 붙이기
-        int offset = Character.isUpperCase(c) ? 'A' : 'a';
-        int position = c - offset;
-        position = (position + n) % ('Z' - 'A' + 1);
-        return (char) (offset + position);
+
+        return result.toString();
     }
 }
