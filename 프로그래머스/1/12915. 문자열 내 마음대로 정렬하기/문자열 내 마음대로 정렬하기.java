5047317312a64619597bb94a47e6028f41dd50a7
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-06 20:24:01 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.10 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -2,12 +2,35 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Arrays.sort(strings, (s1, s2) -> {
-            if (s1.charAt(n) != s2.charAt(n)) { //s1과 s2의 n번째 문자가 다를 때만 해당 문자로 비교
-                return s1.charAt(n) - s2.charAt(n);
+        List<Word> words = new ArrayList<>();
+        
+        for(String str : strings) {
+            char standard = str.charAt(n);
+            words.add(new Word(str, standard));
+        }
+        
+        words.sort((o1, o2) -> {
+            if (o1.standard == o2.standard) {
+                return o1.name.compareTo(o2.name); //이름을 사전 순으로 비교
             }
-            return s1.compareTo(s2); //그 외에는 사전 순 정렬
+            return Character.compare(o1.standard, o2.standard); //기준 문자 비교
         });
-        return strings;
+        
+        String[] answer = new String[words.size()];
+        for (int i = 0; i < words.size(); i++) {
+            answer[i] = words.get(i).name;
+        }
+        
+        return answer;
+    }
+    
+    private class Word {
+        public String name;
+        public char standard;
+        
+        Word(String name, char standard) {
+            this.name = name;
+            this.standard = standard;
+        }
     }
 }
