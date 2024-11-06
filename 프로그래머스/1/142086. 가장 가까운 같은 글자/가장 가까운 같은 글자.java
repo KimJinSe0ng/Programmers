1aafa69e0167e75eb15270ee2543af6ea92695bb
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-06 20:13:21 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.72 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        Map<Character, Integer> lastIndexMap = new HashMap<>();
+        
+        int[] result = new int[s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            
+            if (lastIndexMap.containsKey(currentChar)) {
+                result[i] = i - lastIndexMap.get(currentChar);
+            } else {
+                result[i] = -1;
+            }
+            
+            lastIndexMap.put(currentChar, i);
+        }
+
+        return result;
+    }
+}
