5cd77793db6f45c9521af0a860453ccc5e7d47f8
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-06 14:28:17 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int num) {
+        long number = num;
+
+        if(number == 1) return 0;
+        
+        for(int i = 1; i <= 500; i++) {
+            if(number % 2 == 0) {
+                number /= 2;
+            } else {
+                number = number * 3 + 1;
+            }
+            
+            if(number == 1) return i;
+        }
+        
+        return -1;
+    }
+    
+}
