a075cbfa7f8d5763cd410b0dfd2aaf6c873b9585
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-14 21:56:19 +0900

    [level 2] Title: 삼각 달팽이, Time: 21.32 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,40 +1,27 @@
 class Solution {
+    private static final int[] dx = {0, 1, -1};
+    private static final int[] dy = {1, 0, -1};
     public int[] solution(int n) {
         int[][] t = new int[n][n];
-        
         int x = 0;
         int y = 0;
         int v = 1;
+        int d = 0;
         
         while(true) {
-            //1.밑으로 이동
-            while(true) {
-                t[y][x] = v++;
-                if(y + 1 == n || t[y + 1][x] != 0) break;
-                y += 1;
+            t[y][x] = v++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            //더 이상 진행할 수 없을 때 처리
+            if(nx == n || ny == n || nx == -1 || ny == -1 || t[ny][nx] != 0) {
+                d = (d + 1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+                //전환된 방향으로도 진행을 못하는 경우
+                if(nx == n || ny == n || nx == -1 || ny == -1 || t[ny][nx] != 0) break;
             }
-            if(x + 1 == n || t[y][x + 1] != 0) break;
-            x += 1;
-            
-            //2.오른쪽으로 이동
-            while(true) {
-                t[y][x] = v++;
-                if(x + 1 == n || t[y][x + 1] != 0) break;
-                x += 1;
-            }
-            if(t[y - 1][x - 1] != 0) break;
-            x -= 1;
-            y -= 1;
-            
-            //3.대각선으로 이동
-            while(true) {
-                t[y][x] = v++;
-                if(t[y - 1][x - 1] != 0) break;
-                x -= 1;
-                y -= 1;
-            }
-            if(y + 1 == n || t[y + 1][x] != 0) break;
-            y += 1;
+            x = nx;
+            y = ny;
         }
         
         int[] result = new int[v - 1];
