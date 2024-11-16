1205373a6532221b75c08f750d881a266c6dad65
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-16 13:48:04 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,20 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        StringBuilder ternary = new StringBuilder();
-        while (n > 0) {
-            ternary.append(n % 3);
-            n /= 3;              
-        }
-
-        String reversedTernary = ternary.toString();
-
-        int answer = 0;
-        for (int i = 0; i < reversedTernary.length(); i++) {
-            int digit = reversedTernary.charAt(i) - '0';
-            answer += digit * Math.pow(3, reversedTernary.length() - 1 - i);
-        }
-
-        return answer;
+        String ternary = Integer.toString(n, 3);
+        StringBuilder sb = new StringBuilder(ternary);
+        
+        sb.reverse(); //앞뒤 반전
+        
+        String digit = sb.toString();
+        
+        return Integer.parseInt(digit, 3);
     }
 }
