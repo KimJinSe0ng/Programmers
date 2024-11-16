7b574d29c8e1c88c8b9724876dbcfef2dd099e44
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-17 00:19:48 +0900

    [level 2] Title: 카펫, Time: 9.38 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,15 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        //갈색 가로 = 노랑 가로
-        //갈색 세로 = 노랑 세로 + 2
-        //갈색 개수 : 노랑 가로 * 2 +  (노랑 세로 + 2) * 2
-        int[] answer = new int[2];
-        for(int x = 1; x <= yellow; x++) { //노랑의 가로:x, 노랑의 세로:y
-            int y = yellow / x;
-            if(x < y) {
-                continue;
-            }
-            if(x * 2 + (y + 2) * 2 == brown) {
-                answer[0] = x + 2;
-                answer[1] = y + 2;
-                break;
+        for (int width = 3; width <= 5000; width++) {
+            for (int height = 3; height <= width; height++) {
+                int boundary = (width + height - 2) * 2;
+                int center = width * height - boundary;
+                if (brown == boundary && yellow == center) {
+                    return new int[] {width, height};
+                }
             }
         }
-        return answer;
+        
+        return null;
     }
 }
