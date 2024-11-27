5e47ef8121845e01f4c4e367e66520dc5c0f6312
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-11-27 21:49:04 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 112 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int zero = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        while (N > 0) {
+            zero += N / 5;
+            N /= 5;
+        }
+
+        System.out.println(zero);
+    }
+}
