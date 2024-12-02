b0f4585cbdc149d561dc9e00d9f154329000151b
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-02 18:24:34 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 356 ms, Memory: 37232 KB -BaekjoonHub

---
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static char[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {-1, 1, 0, 0};
+    static int x, y;
+    static int friend = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new char[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String line = st.nextToken();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j);
+                if (map[i][j] == 'I') {
+                    y = i;
+                    x = j;
+                }
+            }
+        }
+
+        bfs(x, y);
+        if (friend == 0) {
+            System.out.println("TT");
+        } else {
+            System.out.println(friend);
+        }
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        visited[y][x] = true;
+        queue.add(new int[]{y, x});
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for (int d = 0; d < 4; d++) {
+                int ny = cur[0] + dy[d];
+                int nx = cur[1] + dx[d];
+                if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
+                    if (!visited[ny][nx] && map[ny][nx] != 'X') {
+                        visited[ny][nx] = true;
+                        queue.add(new int[]{ny, nx});
+                        if (map[ny][nx] == 'P') {
+                            friend++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
