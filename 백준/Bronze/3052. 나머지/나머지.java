93093d4dc8c6da57708bf198f701a7d6e8319d0e
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-04 01:46:00 +0900

    [Bronze II] Title: 나머지, Time: 100 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,18 +1,20 @@
-import java.util.Scanner;
-import java.util.HashSet;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		HashSet<Integer> h = new HashSet<Integer>();
- 
-		for (int i = 0; i < 10; i++) {
-			h.add(in.nextInt() % 42);
-		//입력받은 값의 나머지 값을 add메소드를 통해 HashSet에 저장
-		}
-        
-		in.close();
-		System.out.print(h.size());
-	}
+    private static final int DIVIDER = 42;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        Set<Integer> remainder = new HashSet<>();
+
+        for (int i = 0; i < 10; i++) {
+            st = new StringTokenizer(br.readLine());
+            int num = Integer.parseInt(st.nextToken());
+
+            remainder.add(num % DIVIDER);
+        }
+
+        System.out.println(remainder.size());
+    }
 }
