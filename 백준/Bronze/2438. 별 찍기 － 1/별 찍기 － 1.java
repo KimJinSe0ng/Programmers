1af4de37dd6990bfd1139a177ffed8f3c05adae3
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-03 20:33:47 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,18 +1,17 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
- 
-		int N = in.nextInt();
-		in.close();
- 
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	}
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println("*".repeat(i));
+        }
+    }
 }
