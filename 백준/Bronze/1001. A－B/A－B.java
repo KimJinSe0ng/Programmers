2571963ecf77d27a1a9dd48d2bae1a3f44dc889a
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-03 20:19:42 +0900

    [Bronze V] Title: A-B, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,10 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        System.out.println(a - b);
+    static int A, B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        
+        System.out.println(minus(A, B));
+    }
+
+    private static int minus(int a, int b) {
+        return a - b;
     }
 }
