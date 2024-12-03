5a70b2b6bedc853f936fd25d0b16515c4cc2f128
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-03 21:22:13 +0900

    [Bronze V] Title: 개, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -1,15 +1,5 @@
 public class Main {
-	public static void main(String[] args){
- 
-		StringBuilder sb = new StringBuilder();
- 
-		sb.append("|\\_/|\n");
-		sb.append("|q p|   /}\n");
-		sb.append("( 0 )\"\"\"\\\n");
-		sb.append("|\"^\"`    |\n");    
-		sb.append("||_/=\\\\__|\n"); 
-        
-		System.out.println(sb);
- 
-	}
+    public static void main(String[] args) {
+        System.out.printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+    }
 }
