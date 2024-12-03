db5c5e02574304bc2a6b74804913e6c1ad59a001
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-03 20:40:23 +0900

    [Bronze V] Title: 구구단, Time: 132 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,11 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        for (int i = 1; i < 10; i++) {
-            System.out.println(N + " * " + i + " = " + (N * i));
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d * %d = %d\n", N, i, N * i);
         }
     }
 }
