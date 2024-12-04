22d06c51fc58b25002eed7e27b356f588f2b7857
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-04 15:46:42 +0900

    [Bronze V] Title: 문자와 문자열, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -1,19 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String st = br.readLine();
-		int n = Integer.parseInt(br.readLine())-1;
-		
-		br.close();
-		
-		System.out.println(st.charAt(n));
-		
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String S = st.nextToken();
+
+        st = new StringTokenizer(br.readLine());
+        int i = Integer.parseInt(st.nextToken());
+
+        System.out.println(S.charAt(i - 1));
+    }
 }
