ca0c46a3d6941bea4abfedc7bb0a19cfac8b8c9f
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-04 16:32:12 +0900

    [Bronze II] Title: 음계, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -1,12 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        String[] input = scanner.nextLine().split(" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
         int[] notes = new int[8];
-        
+
         for (int i = 0; i < 8; i++) {
             notes[i] = Integer.parseInt(input[i]);
         }
