884e041e398a86f5375f245c3f399a1109ecd69d
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-04 16:04:13 +0900

    [Bronze III] Title: 알람 시계, Time: 112 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,24 +1,20 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) {
-    
-		Scanner in = new Scanner(System.in);
-		
-		int H = in.nextInt();
-		int M = in.nextInt();
-		in.close();
-		
-		if(M < 45) {
-			H--;		
-			M= 60 - (45 - M); 	
-			if(H < 0) {
-				H = 23;
-			}
-			System.out.println(H + " " + M);
-		}
-		else {
-			System.out.println(H + " " + (M - 45));
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int hour;
+        if ((60 + M - 45) / 60 == 1) {
+            hour = (24 + H) % 24;
+        } else {
+            hour = (24 + H -1) % 24;
+        }
+        int min = (60 + M - 45) % 60;
+        System.out.printf("%d %d\n", hour, min);
+    }
 }
