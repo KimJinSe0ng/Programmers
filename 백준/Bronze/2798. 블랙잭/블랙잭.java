22a52daff08de83eadd6508325d1037c395c3619
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-10 19:20:18 +0900

    [Bronze II] Title: 블랙잭, Time: 112 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,56 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int N, M;
+    static int[] cards;
+    static int maxSum = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
-        //각 카드에는 양의 정수, 딜러는 N장의 카드를 모두 숫자가 보이도록 바닥에 놓는다.
-        //딜러는 숫자 M을 외친다.
-        //플레이어는 N장의 카드 중에서 3장의 카드를 고른다. 고른 카드의 합은 M을 넘지 않으며 최대한 가까이 만들어야 한다.
-        //N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합을 구해 출력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] cards = new int[N];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        cards = new int[N];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < cards.length; i++) {
+        for (int i = 0; i < N; i++) {
             cards[i] = Integer.parseInt(st.nextToken());
         }
-        int result = search(cards, N, M);
-        System.out.println(result);
-    }
 
-    static int search(int[] arr, int N, int M) {
-        int result = 0;
-
-        // 3개를 고르기 때문에 첫번째 카드는 N - 2 까지만 순회
-        for (int i = 0; i < N - 2; i++) {
-
-            // 두 번째 카드는 첫 번째 카드 다음부터 N - 1 까지만 순회
-            for (int j = i + 1; j < N - 1; j++) {
-
-                // 세 번째 카드는 두 번째 카드 다음부터 N 까지 순회
-                for (int k = j + 1; k < N; k++) {
-
-                    // 3개 카드의 합 변수 temp
-                    int temp = arr[i] + arr[j] + arr[k];
-
-                    // M과 세 카드의 합이 같다면 temp return 및 종료
-                    if (M == temp) {
-                        return temp;
-                    }
-
-                    // 세 카드의 합이 이전 합보다 크면서 M 보다 작을 경우 result 갱신
-                    if(result < temp && temp < M) {
-                        result = temp;
+        for (int first = 0; first < cards.length - 2; first++) {
+            for (int second = first + 1; second < cards.length - 1; second++) {
+                for (int third = second + 1; third < cards.length; third++) {
+                    if (cards[first] + cards[second] + cards[third] <= M) {
+                        maxSum = Math.max(maxSum, cards[first] + cards[second] + cards[third]);
                     }
                 }
             }
         }
 
-        // 모든 순회를 마치면 result 리턴
-        return result;
+        System.out.println(maxSum);
     }
 }
