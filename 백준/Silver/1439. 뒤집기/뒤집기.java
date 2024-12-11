6f6c8fe492b5d8aafcd38a1675fd911ec036648e
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-11 23:59:24 +0900

    [Silver V] Title: 뒤집기, Time: 104 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,28 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String S = sc.nextLine();
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String S = st.nextToken();
         int zero = 0;
         int one = 0;
+        boolean isZero = false;
+        boolean isOne = false;
 
-        if (S.charAt(0) == '0') { //초깃값
-            zero++;
-        } else {
-            one++;
-        }
-
-        for (int i = 1; i < S.length(); i++) {
-            if (S.charAt(i - 1) != S.charAt(i)) { //이전 숫자와 다를 경우
-                if (S.charAt(i) == '0') {
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '0') {
+                isOne = false;
+                if (!isZero) {
                     zero++;
-                } else {
+                    isZero = true;
+                }
+            } else {
+                isZero = false;
+                if (!isOne) {
                     one++;
+                    isOne = true;
                 }
             }
         }
-        System.out.println(Math.min(one, zero));
+
+        System.out.println(Math.min(zero, one));
     }
 }
