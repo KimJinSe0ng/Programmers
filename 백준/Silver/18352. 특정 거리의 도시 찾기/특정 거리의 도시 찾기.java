f9612b08bd356e5a0691cb8ac7596f6c6201683c
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-13 18:21:24 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 932 ms, Memory: 254900 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,84 +1,74 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N, M, K, X;
-    static boolean[] visited;
-    static ArrayList<Integer>[] A;
-    static boolean isFind = false;
+    static List<Integer>[] graph;
+    static List<Integer> result = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
-
-        visited = new boolean[N + 1];
-        A = new ArrayList[N + 1];
+        graph = new List[N + 1];
 
         for (int i = 1; i <= N; i++) {
-            A[i] = new ArrayList<>();
+            graph[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 1; i <= M; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            A[s].add(e);
-//            A[e].add(s); //단방향 그래프
+            graph[s].add(e);
         }
 
-        BFS(X);
+        bfs(X);
 
-        if (!isFind) {
+        if (result.isEmpty()) {
             System.out.println(-1);
+        } else {
+            Collections.sort(result);
+            for (int city : result) {
+                System.out.println(city);
+            }
         }
+
     }
 
-    private static void BFS(int start) {
-        Queue<State> queue = new LinkedList<>();
-        queue.add(new State(start, 0));
+    private static void bfs(int start) {
+        Queue<City> queue = new LinkedList<>();
+        boolean[] visited = new boolean[N + 1];
+        queue.add(new City(start, 0));
         visited[start] = true;
-        ArrayList<Integer> result = new ArrayList<>();
 
         while (!queue.isEmpty()) {
-            State now = queue.poll();
-
-            if (now.depth == K) {
-                isFind = true;
-                result.add(now.node);
+            City now = queue.poll();
+            if (now.dist == K) {
+                result.add(now.no);
+            } else if (now.dist > K) {
+                continue;
             }
 
-            for (int next : A[now.node]) {
+            for (int next : graph[now.no]) {
                 if (!visited[next]) {
-                    queue.add(new State(next, now.depth + 1));
                     visited[next] = true;
+                    queue.add(new City(next, now.dist + 1));
                 }
             }
         }
-
-        if (isFind) {
-            result.sort(null);
-            for (int node : result) {
-                System.out.println(node);
-            }
-        }
     }
 
-    private static class State {
-        public final int node;
-        public final int depth;
+    private static class City {
+        public int no;
+        public int dist;
 
-        public State(int node, int depth) {
-            this.node = node;
-            this.depth = depth;
+        public City(int no, int dist) {
+            this.no = no;
+            this.dist = dist;
         }
     }
 }
