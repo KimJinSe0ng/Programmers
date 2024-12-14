1e3d55a8b481e976e7abd0fa0870b74a66fcaaf4
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-14 23:01:05 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,24 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N, K;
-    static int D[][];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
-        D = new int[N + 1][N + 1];
-        for (int i = 0; i <= N; i++) {
-            D[i][1] = i;
-            D[i][0] = 1;
-            D[i][i] = 1;
+        long result = factorial(N) / (factorial(K) * factorial(N - K));
+        
+        System.out.println(result);
+    }
+
+    private static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
         }
-        for (int i = 2; i <= N; i++) {
-            for (int j = 1; j < i; j++) {
-                D[i][j] = D[i - 1][j] + D[i - 1][j - 1]; //조합 기본 점화식
-            }
-        }
-        System.out.println(D[N][K]);
     }
 }
