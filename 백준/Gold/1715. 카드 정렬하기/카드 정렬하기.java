f0b6b2645d3fa2b37a1b8c5d13ec37a592e9369c
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-14 10:32:29 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 436 ms, Memory: 30400 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,33 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> cards = new PriorityQueue<>();
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            priorityQueue.add(Integer.parseInt(st.nextToken()));
+            cards.add(Integer.parseInt(st.nextToken()));
         }
 
-        int num1 = 0;
-        int num2 = 0;
-        int sum = 0;
-        while (priorityQueue.size() != 1) {
-            num1 = priorityQueue.remove();
-            num2 = priorityQueue.remove();
-            sum += num1 + num2;
-            priorityQueue.add(num1 + num2);
+        int totalComparison = 0;
+        while (cards.size() > 1) {
+            int first = cards.poll();
+            int second = cards.poll();
+            int combined = first + second;
+
+            totalComparison += combined;
+            cards.add(combined);
         }
 
-        System.out.println(sum);
+        System.out.println(totalComparison);
+
     }
+
 }
