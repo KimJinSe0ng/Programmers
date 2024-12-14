aed4c544c71f5256b077053a32a91d77b631f879
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-14 13:20:32 +0900

    [Gold V] Title: 경쟁적 전염, Time: 236 ms, Memory: 22488 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.java
@@ -1,76 +1,76 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N, K, S, X, Y;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int[][] map;
-    static PriorityQueue<Virus> queue = new PriorityQueue<>();
-    static int second = 0;
-
-    private static class Virus implements Comparable<Virus> {
-        public final int x;
-        public final int y;
-        public final int value;
-
-        public Virus(int x, int y, int value) {
-            this.x = x;
-            this.y = y;
-            this.value = value;
-        }
-
-        @Override
-        public int compareTo(Virus other) {
-            return Integer.compare(this.value, other.value);
-        }
-    }
+    static int N, K;
+    static int S, X, Y;
+    static int[][] graph;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int time = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        map = new int[N][N];
+        graph = new int[N][N];
+        List<Virus> virusList = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] != 0) {
-                    queue.add(new Virus(i, j, map[i][j]));
+                graph[i][j] = Integer.parseInt(st.nextToken());
+                if (graph[i][j] != 0) {
+                    virusList.add(new Virus(graph[i][j], i, j, 0));
                 }
             }
         }
+
+        virusList.sort(Comparator.comparingInt(v -> v.no));
+
+        Queue<Virus> virusQueue = new LinkedList<>(virusList);
 
         st = new StringTokenizer(br.readLine());
         S = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
         Y = Integer.parseInt(st.nextToken());
 
-        for (int s = 0; s < S; s++) {
-            if (queue.isEmpty()) break;
-            int size = queue.size();
-            PriorityQueue<Virus> nextQueue = new PriorityQueue<>();
-            while (size-- > 0) {
-                Virus now = queue.poll();
-                for (int d = 0; d < 4; d++) {
-                    int nx = now.x + dx[d];
-                    int ny = now.y + dy[d];
-                    if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                        if (map[nx][ny] == 0) {
-                            map[nx][ny] = now.value;
-                            nextQueue.add(new Virus(nx, ny, now.value));
-                        }
-                    }
+        bfs(virusQueue);
+
+        System.out.println(graph[X - 1][Y - 1]);
+    }
+
+    private static void bfs(Queue<Virus> virusQueue) {
+        while (!virusQueue.isEmpty()) {
+            Virus now = virusQueue.poll();
+
+            if (now.time == S) break;
+
+            for (int d = 0; d < 4; d++) {
+                int ny = now.y + dy[d];
+                int nx = now.x + dx[d];
+                if (0 <= ny && ny < N && 0 <= nx && nx < N && graph[ny][nx] == 0) {
+                    graph[ny][nx] = now.no;
+                    virusQueue.add(new Virus(now.no, ny, nx, now.time + 1));
                 }
             }
-            queue = nextQueue;
-        }
 
-        System.out.println(map[X - 1][Y - 1]);
+        }
     }
+
+    private static class Virus {
+        public int no;
+        public int y;
+        public int x;
+        public int time;
+
+        public Virus(int no, int y, int x, int time) {
+            this.no = no;
+            this.y = y;
+            this.x = x;
+            this.time = time;
+        }
+    }
+
 }
