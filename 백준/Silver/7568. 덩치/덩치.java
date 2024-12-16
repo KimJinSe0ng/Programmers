9bb653c56bf2d4286d305a84a1dd849a5b0ac0b6
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2024-12-16 15:25:31 +0900

    [Silver V] Title: 덩치, Time: 112 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,36 +1,43 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
- 
-		int[][] arr = new int[N][2];
- 
-		String[] sp;
-		for(int i = 0; i < N; i++) {
-			sp = br.readLine().split(" ");	
-			arr[i][0] = Integer.parseInt(sp[0]);	
-			arr[i][1] = Integer.parseInt(sp[1]);	
-		}
-		
-				
-		for(int i = 0; i < N; i++) {
-			int rank = 1;
-			
-			for(int j = 0; j < N; j++) {
-				if(i == j) continue;
-				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
-					rank++;
-				}
-			}
- 
-			System.out.print(rank + " ");
-		}
- 
-	}
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int[][] people = new int[N][2];
+        int[] ranks = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int height = Integer.parseInt(st.nextToken());
+            people[i][0] = weight;
+            people[i][1] = height;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int rank = 1;
+
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+
+                if (people[i][0] < people[j][0] && people[i][1] < people[j][1]) {
+                    rank++;
+                }
+            }
+
+            ranks[i] = rank;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int rank : ranks) {
+            sb.append(rank).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+    }
 }
