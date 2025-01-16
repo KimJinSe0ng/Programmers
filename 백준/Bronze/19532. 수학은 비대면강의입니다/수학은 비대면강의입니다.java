1fa43bf4a4e8b096953171a63d89cd463c838028
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-16 13:38:04 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 168 ms, Memory: 17088 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,25 +1,23 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int a, b, c, d, e, f;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        f = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
 
         for (int x = -999; x <= 999; x++) {
             for (int y = -999; y <= 999; y++) {
-                if (a * x + b * y == c) {
-                    if (d * x + e * y == f) {
-                        System.out.printf("%d %d\n", x, y);
-                    }
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    System.out.printf("%d %d\n", x, y);
                 }
             }
         }
