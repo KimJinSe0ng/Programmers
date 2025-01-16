a730ad639b18a9db44c59d73df45287b4a268773
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-16 14:09:09 +0900

    [Silver III] Title: 숫자 야구, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -1,78 +1,81 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[][] question;
+    static int[][] hints;
     static int answer = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
-        question = new int[N][3];
+        hints = new int[N][3];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            question[i][0] = Integer.parseInt(st.nextToken());
-            question[i][1] = Integer.parseInt(st.nextToken());
-            question[i][2] = Integer.parseInt(st.nextToken());
+            hints[i][0] = Integer.parseInt(st.nextToken());
+            hints[i][1] = Integer.parseInt(st.nextToken());
+            hints[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        for (int num100 = 1; num100 <= 9; num100++) {
-            for (int num10 = 1; num10 <= 9; num10++) {
-                for (int num1 = 1; num1 <= 9; num1++) {
-                    if (num1 == num10 || num10 == num100 || num100 == num1) {
+        int count = 0;
+        for (int a = 1; a <= 9; a++) {
+            for (int b = 1; b <= 9; b++) {
+                for (int c = 1; c <= 9; c++) {
+                    if (a == b || b == c || c == a) {
                         continue;
                     }
 
-                    int passedHintCount = 0;
+                    int correctCount = 0;
 
-                    for (int i = 0; i < N; i++) {
-                        int guess = question[i][0];
-                        int strike = question[i][1];
-                        int ball = question[i][2];
+                    for (int[] hint : hints) {
+                        int number = hint[0];
+                        int strike = hint[1];
+                        int ball = hint[2];
 
-                        int guess100 = guess / 100;
-                        int guess10 = (guess % 100) / 10;
-                        int guess1 = guess % 10;
+                        int number100 = number / 100;
+                        int number10 = (number / 10) % 10;
+                        int number1 = number % 10;
 
                         int strike_count = 0;
                         int ball_count = 0;
 
-                        if (num100 == guess100) {
+                        if (number100 == a) {
                             strike_count++;
                         }
-                        if (num10 == guess10) {
+                        if (number10 == b) {
                             strike_count++;
                         }
-                        if (num1 == guess1) {
+                        if (number1 == c) {
                             strike_count++;
                         }
 
-                        if (num100 == guess10 || num100 == guess1) {
+                        if (number100 == b || number100 == c) {
                             ball_count++;
                         }
-                        if (num10 == guess100 || num10 == guess1) {
+                        if (number10 == a || number10 == c) {
                             ball_count++;
                         }
-                        if (num1 == guess100 || num1 == guess10) {
+                        if (number1 == b || number1 == a) {
                             ball_count++;
                         }
 
                         if (strike_count == strike && ball_count == ball) {
-                            passedHintCount++;
+                            correctCount++;
                         }
                     }
 
-                    if (passedHintCount == N) {
-                        answer++;
+                    if (correctCount == N) {
+                        count++;
                     }
                 }
             }
         }
-        System.out.println(answer);
+
+        System.out.println(count);
     }
 }
