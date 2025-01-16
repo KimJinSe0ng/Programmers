1271d7fcf2a399aaf48b8a1e456b0b8d1b94b9f0
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-16 13:33:19 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 112 ms, Memory: 14452 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.java
@@ -1,23 +1,25 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int answer = 0;
-    public static void main(String[] args) throws IOException{
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
 
-        for (int A = 0; A <= N; A++) { //택희
-            for (int B = 0; B <= N; B++) { //영훈
-                for (int C = 0; C <= N; C++) { //남규
-                    if (A + B + C == N) { //남는 사탕 없어야 함
-                        if (C >= B + 2) { //남규는 영훈이보다 2개 이상
-                            if (A != 0 && B != 0 && C != 0) { //셋 중 사탕을 0개 받는 사람 없어야 함
-                                if (A % 2 != 1) { //택희의 사탕 수는 짝수여야 함
-                                    answer++;
+        int count = 0;
+        for (int A = 0; A < N; A++) { //택희
+            for (int B = 0; B < N; B++) { //영훈
+                for (int C = 0; C < N; C++) { //남규
+                    if (A + B + C == N) {
+                        if (C >= B + 2) {
+                            if (A != 0 && B != 0 && C != 0) {
+                                if (A % 2 != 1) {
+                                    count++;
                                 }
                             }
                         }
@@ -25,6 +27,7 @@ public class Main {
                 }
             }
         }
-        System.out.println(answer);
+
+        System.out.println(count);
     }
 }
