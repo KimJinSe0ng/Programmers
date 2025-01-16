1e84992fbec5793083932e021554cfe7b30091db
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-16 13:24:09 +0900

    [Bronze I] Title: 암호 키, Time: 196 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/1816. 암호 키/암호 키.java
+++ 백준/Bronze/1816. 암호 키/암호 키.java
@@ -1,29 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            long S = Long.parseLong(st.nextToken());
-            if (isPrime(S)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
-            }
+            long number = Long.parseLong(st.nextToken());
+            if (isCorrectPassword(number)) System.out.println("YES");
+            else System.out.println("NO");
         }
     }
 
-    private static boolean isPrime(long S) {
-        for (int i = 2; i <= 1000000; i++) {
-            if (S % i == 0) { //나눠지면 소수이고, 10^6보다 작으면 NO
+    private static boolean isCorrectPassword(long number) {
+        for (int i = 2; i <= 1_000_000; i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
