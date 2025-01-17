38caa953ace0219281d0af58f0ffcaee8e71d389
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-17 09:44:21 +0900

    [Platinum IV] Title: 체커, Time: 228 ms, Memory: 23956 KB -BaekjoonHub

--- 백준/Platinum/1090. 체커/체커.java
+++ 백준/Platinum/1090. 체커/체커.java
@@ -1,69 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int[][] checkers;
-//    static int min_x = Integer.MAX_VALUE;
-//    static int max_x = Integer.MIN_VALUE;
-//    static int min_y = Integer.MAX_VALUE;
-//    static int max_y = Integer.MIN_VALUE;
-    static int[] answer;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        checkers = new int[N][2];
-        answer = new int[N];
-        int[] arr_x = new int[N]; //x좌표
-        int[] arr_y = new int[N]; //y좌표
+        int N = Integer.parseInt(st.nextToken());
+        List<int[]> points = new ArrayList<>();
+        List<Integer> arrX = new ArrayList<>();
+        List<Integer> arrY = new ArrayList<>();
+        int[] answer = new int[N];
         Arrays.fill(answer, -1);
-        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            checkers[i][0] = Integer.parseInt(st.nextToken());
-            checkers[i][1] = Integer.parseInt(st.nextToken());
-            arr_x[i] = checkers[i][0];
-            arr_y[i] = checkers[i][1];
-//            min_x = Math.min(x, min_x);
-//            min_y = Math.min(y, min_y);
-//            max_x = Math.max(x, max_x);
-//            max_y = Math.max(y, max_y);
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points.add(new int[]{x, y});
+            arrX.add(x);
+            arrY.add(y);
         }
 
-        int[] temp = new int[N];
-
-        for (int x : arr_x)
-        {
-            for (int y : arr_y) {
-                for (int i = 0; i < checkers.length; i++) {
-                    temp[i] = Math.abs(x - checkers[i][0]) + Math.abs(y - checkers[i][1]);
+        // 만날 장소 정하기
+        for (int x : arrX) {
+            for (int y : arrY) {
+                // 만날 장소까지 모든 점의 거리 계산
+                List<Integer> dist = new ArrayList<>();
+                for (int[] point : points) {
+                    int px = point[0];
+                    int py = point[1];
+                    int d = Math.abs(px - x) + Math.abs(py - y);
+                    dist.add(d);
                 }
 
-                Arrays.sort(temp); //짧은 거리 순으로 정렬되어 있음
-                
+                // 짧은 거리 순으로 정렬
+                Collections.sort(dist);
+
                 int sum = 0;
-                for (int i = 0; i < temp.length; i++) {
-                    sum += temp[i];
-                    if (answer[i] == -1) {
-                        answer[i] = sum;
-                    } else {
+                for (int i = 0; i < dist.size(); i++) {
+                    sum += dist.get(i);
+                    if (answer[i] != -1) {
                         answer[i] = Math.min(answer[i], sum);
+                    } else {
+                        answer[i] = sum;
                     }
                 }
             }
         }
 
-        for (int i : answer) {
-            sb.append(i).append(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int a : answer) {
+            sb.append(a).append(" ");
         }
-
         System.out.println(sb.toString().trim());
     }
 }
