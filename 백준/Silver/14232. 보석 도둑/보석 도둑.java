57c09339a9bfbbb56635338a7923b1d155791747
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-17 10:28:38 +0900

    [Silver I] Title: 보석 도둑, Time: 120 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Silver/14232. 보석 도둑/보석 도둑.java
+++ 백준/Silver/14232. 보석 도둑/보석 도둑.java
@@ -2,39 +2,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long k = Long.parseLong(br.readLine());
 
-	public static void main(String[] args)throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long k = Long.parseLong(br.readLine());
-		
-		int c = 2;
-		int cnt = 0;
-		List<String> list = new ArrayList<>();
-		while(k != 1) {
-			if (c>=1000000) {
-				list.add(k+"");
-				cnt++;
-				break;
-			}
-			if (k % c == 0) {
-				k/=c;
-				cnt++;
-				list.add(c+"");
-			}
-			else {
-				c++;
-			}
-		}
-		
-		
-		System.out.println(cnt);
-		System.out.println(String.join(" ", list));
-		
-	}
-	
-	
+        List<Long> factors = primeFactorization(k);
 
+        System.out.println(factors.size());
+        for (long factor : factors) {
+            System.out.print(factor + " ");
+        }
+    }
+
+    private static List<Long> primeFactorization(long n) {
+        List<Long> factors = new ArrayList<>();
+
+        for (long i = 2; i * i <= n; i++) {
+            while (n % i == 0) {
+                factors.add(i);
+                n /= i;
+            }
+        }
+
+        if (n > 1) {
+            factors.add(n);
+        }
+
+        Collections.sort(factors);
+        return factors;
+    }
 }
