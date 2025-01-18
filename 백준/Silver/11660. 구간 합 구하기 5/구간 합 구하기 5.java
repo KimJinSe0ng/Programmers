8574aef05b282956fb3b1215dcee83a45878bb37
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-18 17:05:34 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 740 ms, Memory: 121772 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,37 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+    static int[][] map;
+    static int[][] prefix;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int A[][] = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        prefix = new int[N + 1][N + 1];
+
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                A[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int D[][] = new int[N + 1][N + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                // 구간 합 구하기
-                D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
+
+        for (int y = 1; y <= N; y++) {
+            for (int x = 1; x <= N; x++) {
+                prefix[y][x] = prefix[y - 1][x] + prefix[y][x - 1] - prefix[y - 1][x - 1] + map[y - 1][x - 1];
             }
         }
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
+            int x1 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            // 구간 합 배열로 질의에 답변하기
-            int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
-            System.out.println(result);
+            int x2 = Integer.parseInt(st.nextToken());
+
+            int answer = prefix[y2][x2] - prefix[y1 - 1][x2] - prefix[y2][x1 - 1] + prefix[y1 - 1][x1 - 1];
+            sb.append(answer).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
