6a8344257c54b9bfe11417094df963222f4db295
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-20 12:24:29 +0900

    [Silver III] Title: 숫자 야구, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main { //재귀
     static int N;
     static int[][] hints;
     static int answer = 0;
@@ -22,60 +22,52 @@ public class Main {
             hints[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        int count = 0;
-        for (int a = 1; a <= 9; a++) {
-            for (int b = 1; b <= 9; b++) {
-                for (int c = 1; c <= 9; c++) {
-                    if (a == b || b == c || c == a) {
-                        continue;
-                    }
+        recur(0, 100);
+        System.out.println(answer);
 
-                    int correctCount = 0;
+    }
 
-                    for (int[] hint : hints) {
-                        int number = hint[0];
-                        int strike = hint[1];
-                        int ball = hint[2];
-
-                        int number100 = number / 100;
-                        int number10 = (number / 10) % 10;
-                        int number1 = number % 10;
-
-                        int strike_count = 0;
-                        int ball_count = 0;
-
-                        if (number100 == a) {
-                            strike_count++;
-                        }
-                        if (number10 == b) {
-                            strike_count++;
-                        }
-                        if (number1 == c) {
-                            strike_count++;
-                        }
-
-                        if (number100 == b || number100 == c) {
-                            ball_count++;
-                        }
-                        if (number10 == a || number10 == c) {
-                            ball_count++;
-                        }
-                        if (number1 == b || number1 == a) {
-                            ball_count++;
-                        }
-
-                        if (strike_count == strike && ball_count == ball) {
-                            correctCount++;
-                        }
-                    }
-
-                    if (correctCount == N) {
-                        count++;
-                    }
-                }
-            }
+    private static void recur(int idx, int num) {
+        if (idx == N) {
+            answer++;
+            recur(0, num + 1);
+            return;
         }
 
-        System.out.println(count);
+        if (num == 1000) return;
+
+        if (checker(idx, num)) recur(idx + 1, num);
+        else recur(0, num + 1);
+    }
+
+    private static boolean checker(int idx, int num) {
+        int hintNumber = hints[idx][0];
+        int strike = hints[idx][1];
+        int ball = hints[idx][2];
+
+        int strike_count = 0;
+        int ball_count = 0;
+
+        int hint100 = hintNumber / 100;
+        int hint10 = (hintNumber % 100) / 10;
+        int hint1 = hintNumber % 10;
+
+        int guess100 = num / 100;
+        int guess10 = (num % 100) / 10;
+        int guess1 = num % 10;
+
+        if (guess100 == 0 || guess10 == 0 || guess1 == 0) return false;
+
+        if (guess100 == guess10 || guess10 == guess1 || guess1 == guess100) return false;
+
+        if (guess100 == hint100) strike_count++;
+        if (guess10 == hint10) strike_count++;
+        if (guess1 == hint1) strike_count++;
+
+        if (guess100 == hint10 || guess100 == hint1) ball_count++;
+        if (guess10 == hint100 || guess10 == hint1) ball_count++;
+        if (guess1 == hint100 || guess1 == hint10) ball_count++;
+
+        return strike == strike_count && ball == ball_count;
     }
 }
