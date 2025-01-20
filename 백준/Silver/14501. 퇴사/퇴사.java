d936c35e9f0e87bcc0d09bf983d31f9a96abf1f5
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-20 14:52:05 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -3,37 +3,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main { // 재귀
+    static int N;
+    static int[][] interview;
+    static int answer = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        // 첫 번째 줄에 N이 주어집니다.
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        interview = new int[N][2];
 
-        int[][] table = new int[N][2]; // 인터뷰 정보 저장
-        long[] dp = new long[N + 1]; // 최대 수익 저장
-
-        // 인터뷰 정보 입력 받기
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            table[i][0] = Integer.parseInt(st.nextToken()); // 인터뷰 기간
-            table[i][1] = Integer.parseInt(st.nextToken()); // 인터뷰 수익
+            st = new StringTokenizer(br.readLine());
+            interview[i][0] = Integer.parseInt(st.nextToken());
+            interview[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        // 바텀업 DP 계산
-        for (int idx = N - 1; idx >= 0; idx--) {
-            int duration = table[idx][0];
-            int profit = table[idx][1];
+        recur(0, 0);
+        System.out.println(answer);
+    }
 
-            // 인터뷰를 할 수 있는 경우와 없는 경우를 나눠서 계산
-            if (idx + duration > N) {
-                dp[idx] = dp[idx + 1];
-            } else {
-                dp[idx] = Math.max(dp[idx + duration] + profit, dp[idx + 1]);
+    public static void recur(int day, int price) {
+        if (day == N) {
+            if (price >= answer) {
+                answer = price;
             }
+            return;
         }
 
-        // 결과 출력
-        System.out.println(dp[0]);
+        if (day > N) {
+            return;
+        }
+
+        // 상담을 한다면
+        recur(day + interview[day][0], price + interview[day][1]);
+        // 상담을 안 한다면
+        recur(day + 1, price);
     }
 }
