d5b815d470e4fb0fc9fc413642d5f57411c72f10
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-20 15:43:11 +0900

    [Bronze I] Title: 단어 공부, Time: 248 ms, Memory: 21160 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,38 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int[] alpha = new int[26];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine().trim().toUpperCase();
 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		String a = br.readLine();
-		int[] alpa = new int[26];
-		
-		for(int i =0; i < a.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                alpha[c - 'A']++;
+            }
+        }
 
-			if( a.charAt(i) >= 'A' && a.charAt(i) <= 'Z') {
-				alpa[a.charAt(i) - 'A'] += 1;
-			}else {
-				alpa[a.charAt(i)-'a'] += 1;
-			}
-		}
-		
-		int max = -1;
-		char result = '?';
-		
-		for(int i = 0; i < alpa.length; i++) {
-			if(alpa[i] > max) {
-				max = alpa[i];
-				result = (char)(i+65);
-			}else if(alpa[i] == max) {
-				result = '?';
-			}
-		}
-		
-		System.out.println(result);
-	}
+        int maxCount = 0;
+        char maxChar = '?';
+        boolean isDuplicate = false;
 
+        for (int i = 0; i < alpha.length; i++) {
+            if (alpha[i] > maxCount) {
+                maxCount = alpha[i];
+                maxChar = (char) (i + 'A');
+                isDuplicate = false;
+            } else if (alpha[i] == maxCount) {
+                isDuplicate = true;
+            }
+        }
+
+        if (isDuplicate) {
+            System.out.println("?");
+        } else {
+            System.out.println(maxChar);
+        }
+    }
 }
