e167ff3595280b1a47cc696cb95aae8b624ad007
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-20 10:46:09 +0900

    [Silver III] Title: N과 M (4), Time: 144 ms, Memory: 16616 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,38 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N, M;
-    static StringBuilder sb;
-    static boolean[] visited;
-    static int[] A;
+    static List<Integer> numbers = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        sb = new StringBuilder();
-        visited = new boolean[N + 1];
-        A = new int[M];
 
         recur(1, 0);
-        System.out.println(sb.toString());
+
+        System.out.println(sb);
     }
 
-    private static void recur(int at, int len) {
-        if (len == M) {
-            for (int num : A) {
+    private static void recur(int at, int depth) {
+        if (depth == M) {
+            for(int num : numbers) {
                 sb.append(num).append(" ");
             }
             sb.append("\n");
             return;
         }
         for (int i = at; i <= N; i++) {
-            A[len] = i;
-            recur(i, len + 1);
+            numbers.add(i);
+            recur(i, depth + 1);
+            numbers.remove(numbers.size() - 1);
         }
     }
 }
