ce4e41a2a92a9cf279e8b92689863e04bd58cc06
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-20 14:21:39 +0900

    [Gold IV] Title: 다이어트, Time: 144 ms, Memory: 19160 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.java
+++ 백준/Gold/19942. 다이어트/다이어트.java
@@ -1,67 +1,74 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N;
     static int[] necessary;
-    static int[][] items;
-    static int answer = Integer.MAX_VALUE;
-    static ArrayList<Integer> use;
-    static ArrayList<Integer> answer_use = new ArrayList<>();
+    static int[][] ingredient;
+    static int cost = Integer.MAX_VALUE;
+    static List<Integer> used = new ArrayList<>();
+    static List<Integer> answerUsed = new ArrayList<>();
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
+        ingredient = new int[N][5];
         necessary = new int[4];
-        items = new int[N][5];
-        use = new ArrayList<>();
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < necessary.length; i++) {
             necessary[i] = Integer.parseInt(st.nextToken());
         }
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            items[i][0] = Integer.parseInt(st.nextToken());
-            items[i][1] = Integer.parseInt(st.nextToken());
-            items[i][2] = Integer.parseInt(st.nextToken());
-            items[i][3] = Integer.parseInt(st.nextToken());
-            items[i][4] = Integer.parseInt(st.nextToken());
+            ingredient[i][0] = Integer.parseInt(st.nextToken());
+            ingredient[i][1] = Integer.parseInt(st.nextToken());
+            ingredient[i][2] = Integer.parseInt(st.nextToken());
+            ingredient[i][3] = Integer.parseInt(st.nextToken());
+            ingredient[i][4] = Integer.parseInt(st.nextToken());
         }
 
         recur(0, 0, 0, 0, 0, 0);
 
-        if (!answer_use.isEmpty()) {
-            Collections.sort(answer_use);
-            System.out.println(answer);
-            for (Integer idx : answer_use) {
-                System.out.printf("%d ", idx + 1);
+        if (!answerUsed.isEmpty()) {
+            Collections.sort(answerUsed);
+            System.out.println(cost);
+            for (int idx : answerUsed) {
+                System.out.print((idx) + " ");
             }
         } else {
             System.out.println(-1);
         }
+
     }
 
-    private static void recur(int idx, int dan, int zi, int tan, int bi, int price) {
-        if (dan >= necessary[0] && zi >= necessary[1] && tan >= necessary[2] && bi >= necessary[3]) {
-            if (answer > price) {
-                answer = price;
-                answer_use = new ArrayList<>(use);
+    private static void recur(int idx, int p, int f, int s, int v, int c) {
+        if (p >= necessary[0] && f >= necessary[1] && s >= necessary[2] && v >= necessary[3]) {
+            if (cost > c) {
+                cost = c;
+                answerUsed = new ArrayList<>(used);
             }
         }
-        if (idx == N) { //모든 재료를 다 사용했다면
+
+        if (idx == N) {
             return;
         }
 
-        //현재 재료를 선택한 경우
-        use.add(idx);
-        recur(idx + 1, dan + items[idx][0], zi + items[idx][1], tan + items[idx][2], bi + items[idx][3], price + items[idx][4]);
-        use.remove(use.size() - 1);
-        //현재 재료를 선택하지 않은 경우
-        recur(idx + 1, dan, zi, tan, bi, price);
-
+        // 식재료를 사용하는 경우
+        used.add(idx + 1);
+        recur(idx + 1, p + ingredient[idx][0], f + ingredient[idx][1], s + ingredient[idx][2], v + ingredient[idx][3], c + ingredient[idx][4]);
+        used.remove(used.size() - 1);
+        // 식재료를 사용하지 않는 경우
+        recur(idx + 1, p, f, s, v, c);
     }
+
+
 }
