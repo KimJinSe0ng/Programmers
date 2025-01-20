f96cfc87474cab271a5a3bf3aeb06cd3c538d85f
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-20 13:02:45 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,41 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int N;
-    static int[][] items;
+    static int[][] ingredient;
     static int answer = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-        items = new int[N][2];
+        ingredient = new int[N][2];
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            items[i][0] = Integer.parseInt(st.nextToken());
-            items[i][1] = Integer.parseInt(st.nextToken());
+            ingredient[i][0] = Integer.parseInt(st.nextToken());
+            ingredient[i][1] = Integer.parseInt(st.nextToken());
         }
 
         recur(0, 1, 0, 0);
         System.out.println(answer);
+
     }
 
-    private static void recur(int idx, int s, int b, int used) {
+    private static void recur(int idx, int sour, int bitter, int used) {
         if (idx == N) {
             if (used == 0) {
                 return;
             }
-            answer = Math.min(answer, Math.abs(s - b));
+            answer = Math.min(answer, Math.abs(sour - bitter));
             return;
         }
 
-        //재료를 사용한 경우
-        recur(idx + 1, s * items[idx][0], b + items[idx][1], used + 1);
-        //재료를 사용하지 않은 경우
-        recur(idx + 1, s, b, used);
+        // 재료를 사용한 경우
+        recur(idx + 1, sour * ingredient[idx][0], bitter + ingredient[idx][1], used + 1);
+
+        // 재료를 사용하지 않은 경우
+        recur(idx + 1, sour, bitter, used);
     }
 }
