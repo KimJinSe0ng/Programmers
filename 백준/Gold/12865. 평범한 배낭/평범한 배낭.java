e8f102113f4f7e8cc940525e17976c0377afd76d
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-21 16:50:26 +0900

    [Gold V] Title: 평범한 배낭, Time: 224 ms, Memory: 54424 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,37 +4,48 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main { // 탑 다운 dp
     static int N, B;
     static int[][] items;
+    static int answer = 0;
     static int[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
-
-        items = new int[N + 1][2];
-        dp = new int[N + 1][B + 1]; //물건의 개수, 가방의 무게에 따른 경우를 나눔
-
-        for (int i = 1; i < N + 1; i++) {
+        items = new int[N][2];
+        dp = new int[N][100_001];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             items[i][0] = Integer.parseInt(st.nextToken());
             items[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        for (int idx = 1; idx < N + 1; idx++) { //물건 개수1~N
-            for (int weight = 1; weight < B + 1; weight++) { //배낭 무게 1~N
-                if (weight < items[idx][0]) { //현재 배낭의 무게 < 현재 물건의 무게 : 가방에 담을 수 없음
-                    dp[idx][weight] = dp[idx - 1][weight];
-                } else { //현재 배낭의 무게 > 현재 물건의 무게 : 가방에 담을 수 있음
-                    dp[idx][weight] = Math.max(dp[idx - 1][weight], dp[idx - 1][weight - items[idx][0]] + items[idx][1]);
-                }
-            }
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
         }
 
-        System.out.println(dp[N][B]);
+        recur(0, 0);
+        System.out.println(dp[0][0]);
+    }
 
+    private static int recur(int idx, int weight) {
+        if (weight > B) {
+            return Integer.MIN_VALUE;
+        }
+
+        if (idx == N) {
+            return 0;
+        }
+
+        if (dp[idx][weight] != -1) {
+            return dp[idx][weight];
+        }
+
+        dp[idx][weight] = Math.max(recur(idx + 1, weight + items[idx][0]) + items[idx][1], recur(idx + 1, weight));
+
+        return dp[idx][weight];
     }
 }
