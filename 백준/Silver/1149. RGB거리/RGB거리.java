cd431396688711ddf6c24ef7e77cf06bef6cd4b7
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-21 17:10:41 +0900

    [Silver I] Title: RGB거리, Time: 120 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,10 +1,14 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N;
     static int[][] colors;
     static int[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,24 +18,22 @@ public class Main {
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
-            colors[i][0] = Integer.parseInt(st.nextToken());
-            colors[i][1] = Integer.parseInt(st.nextToken());
-            colors[i][2] = Integer.parseInt(st.nextToken());
+            colors[i][0] = Integer.parseInt(st.nextToken()); // 빨
+            colors[i][1] = Integer.parseInt(st.nextToken()); // 초
+            colors[i][2] = Integer.parseInt(st.nextToken()); // 파
         }
 
-        for (int i = 0; i < 3; i++) { //첫 번째 집 초기화, 초기화를 하지 않는다면, 밑에 for문 idx = 1부터 시작
+        for (int i = 0; i < 3; i++) {
             dp[1][i] = colors[1][i];
         }
 
         for (int idx = 2; idx <= N; idx++) {
-            for (int rgb = 0; rgb < 3; rgb++) { //rgb는 현재 선택한 색상
-                if (rgb == 0) { //현재 빨강을 칠한 상황에선 나머지 비용 중 작은 걸 비용으로 합치겠다.
+            for (int rgb = 0; rgb < 3; rgb++) {
+                if (rgb == 0) {
                     dp[idx][rgb] = Math.min(dp[idx - 1][1], dp[idx - 1][2]) + colors[idx][rgb];
-                }
-                if (rgb == 1) {
+                } else if (rgb == 1) {
                     dp[idx][rgb] = Math.min(dp[idx - 1][0], dp[idx - 1][2]) + colors[idx][rgb];
-                }
-                if (rgb == 2) {
+                } else {
                     dp[idx][rgb] = Math.min(dp[idx - 1][0], dp[idx - 1][1]) + colors[idx][rgb];
                 }
             }
@@ -39,6 +41,5 @@ public class Main {
 
         int result = Math.min(Math.min(dp[N][0], dp[N][1]), dp[N][2]);
         System.out.println(result);
-
     }
 }
