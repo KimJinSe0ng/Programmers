38801bcb28c289770b71aeb2e8715cbdb838233a
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-22 12:31:10 +0900

    [Silver IV] Title: 주몽, Time: 168 ms, Memory: 16444 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,40 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main { //1940
+public class Main {
+    static int N, M;
+    static int[] ingredients;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int[] numbers = new int[n];
-
+        N = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-
+        M = Integer.parseInt(st.nextToken());
+        ingredients = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            ingredients[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(numbers);
 
-        int start_index = 0;
-        int end_index = n - 1;
+        Arrays.sort(ingredients);
+
+        int left = 0;
+        int right = ingredients.length - 1;
         int count = 0;
-        while (start_index < end_index) {
-            if (numbers[start_index] + numbers[end_index] < m) {
-                start_index++;
-            } else if (numbers[start_index] + numbers[end_index] > m) {
-                end_index--;
+
+        while (left < right) {
+            if (ingredients[left] + ingredients[right] < M) {
+                left++;
+            } else if (ingredients[left] + ingredients[right] > M) {
+                right--;
             } else {
+                left++;
+                right--;
                 count++;
-                start_index++;
-                end_index--;
             }
         }
+
         System.out.println(count);
-        br.close();
     }
 }
