a8d161b67adbefbad7688df081d480ae9c7dd7dd
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-22 20:17:49 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 53340 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,22 +1,27 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		Queue<Integer> queue = new LinkedList<>();
-		
-		int N = Integer.parseInt(br.readLine());	
-		
-		for(int i=1; i<=N; i++)
-			queue.offer(i);
-		
-		while(queue.size()>1) {
-			queue.poll();
-			queue.offer(queue.poll());
-		}
-		
-		System.out.println(queue.poll());
-	}
+    static int N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        Queue<Integer> cards = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            cards.add(i);
+        }
+
+        while (cards.size() != 1) {
+            int removedCard = cards.poll();
+            int addCard = cards.poll();
+            cards.add(addCard);
+        }
+
+        System.out.println(cards.poll());
+    }
 }
