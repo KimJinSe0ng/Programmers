bd1b37ccd0fcfb674a2749bdfd4aeeae17f12108
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-22 20:36:23 +0900

    [Silver I] Title: 절댓값 힙, Time: 348 ms, Memory: 30888 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,32 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> { //비교할 객체 커스터마이징
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-            if (first_abs == second_abs) { //절댓값이 같은 경우 음수 우선
-                return o1 > o2 ? 1 : -1; //첫번째 애가 크면 1을 리턴, 아니면 -1을 리턴(양수 리턴, 음수 리턴)
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] command = new int[N];
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            if (Math.abs(o1) != Math.abs(o2)) { // 1. 절댓값이 작은 순서
+                return Math.abs(o1) - Math.abs(o2);
             }
-            return first_abs - second_abs; //앞의 데이터가 크면 양수 리턴, 뒤에가 크면 음수 리턴 -> 절댓값 작은 데이터 우선
+            return o1 - o2; // 2. 절댓값이 같으면 숫자가 작은 순서
         });
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
-            int request = Integer.parseInt(br.readLine());
-            if (request == 0) {
-                if (queue.isEmpty()) {
-                    System.out.println("0");
-                } else {
-                    System.out.println(queue.poll());
-                }
+            st = new StringTokenizer(br.readLine());
+            command[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < command.length; i++) {
+            int c = command[i];
+            if (c != 0) {
+                pq.add(c);
             } else {
-                queue.add(request);
+                if (pq.isEmpty()) {
+                    sb.append(0);
+                } else {
+                    sb.append(pq.poll());
+                }
+                sb.append("\n");
             }
         }
+        System.out.println(sb.toString().trim());
     }
 }
