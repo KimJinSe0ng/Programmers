495b1614f6c7648d3781cd9779f1093a88877033
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-25 15:51:54 +0900

    [Silver V] Title: 숫자 카드, Time: 940 ms, Memory: 147584 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -2,58 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] cards;
-    static int M;
-    static int[] guess;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()); 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        cards = new int[N];
+        Set<Integer> cardSet = new HashSet<>(); 
+
         for (int i = 0; i < N; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
+            cardSet.add(Integer.parseInt(st.nextToken()));
         }
-        st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        guess = new int[M];
-        for (int i = 0; i < M; i++) {
-            guess[i] = Integer.parseInt(st.nextToken());
-        }
-        StringBuilder sb = new StringBuilder();
 
-        Arrays.sort(cards);
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < M; i++) {
-            if (binarySearch(guess[i])) {
-                sb.append("1");
-            } else {
-                sb.append("0");
-            }
-            sb.append(" ");
+            int num = Integer.parseInt(st.nextToken());
+            bw.write((cardSet.contains(num) ? "1" : "0") + " ");
         }
 
-        System.out.println(sb.toString().trim());
-    }
-
-    private static boolean binarySearch(int target) {
-        int start = 0;
-        int end = cards.length - 1;
-
-        while (start <= end) {
-            int mid = (start + end) / 2;
-
-            if (cards[mid] < target) {
-                start = mid + 1;
-            } else if (cards[mid] > target) {
-                end = mid - 1;
-            } else {
-                return true;
-            }
-        }
-
-        return false;
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
