c5c51b01f70a261997adf173703e70895b07a693
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-25 17:42:01 +0900

    [Gold V] Title: 신기한 소수, Time: 104 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,49 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        dfs(2, 1);
-        dfs(3, 1);
-        dfs(5, 1);
-        dfs(7, 1);
+        N = Integer.parseInt(br.readLine());
+
+        int[] oneDigitPrimes = {2, 3, 5, 7};
+        for (int prime : oneDigitPrimes) {
+            dfs(prime, 1);
+        }
     }
 
     private static void dfs(int num, int digit) {
         if (digit == N) {
-            if (isPrime(num)) {
-                System.out.println(num);
-            }
+            System.out.println(num);
             return;
         }
-        for (int i = 0; i < 10; i++) {
-            if (i % 2 == 0) {
-                continue;
-            }
-            if (isPrime(num * 10 + i)) {
-                dfs(num * 10 + i, digit + 1);
+
+        for (int i = 1; i <= 9; i++) {
+            int next = num * 10 + i;
+            if (isPrime(next)) {
+                dfs(next, digit + 1);
             }
         }
     }
 
-    private static boolean isPrime(int num) {
-        for (int i = 2; i * i <= num; i++) { //방법1
-            if (num % i == 0) {
+    private static boolean isPrime(int number) {
+        if (number < 2) return false;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
         return true;
-//        for (int i = 2; i <= num / 2; i++) { //방법2
-//            if (num % i == 0) {
-//                return false;
-//            }
-//        }
-//        return true;
     }
 }
