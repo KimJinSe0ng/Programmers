45f007c6a4ef48eaf24f25cace62fd55ce543f3c
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-26 13:05:47 +0900

    [Silver IV] Title: 수 찾기, Time: 1240 ms, Memory: 52956 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,49 +2,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int[] A;
+    static int M;
+    static int[] guess;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(bf.readLine());
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		int[] numbers = new int[n];
-		for (int i = 0; i < n; i++) {
-			numbers[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(numbers);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        guess = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            guess[i] = Integer.parseInt(st.nextToken());
+        }
 
-		int m = Integer.parseInt(bf.readLine());
-		st = new StringTokenizer(bf.readLine());
-		int num;
+        Arrays.sort(A);
 
-		for (int i = 0; i < m; i++) {
-			num = Integer.parseInt(st.nextToken());
-			if(binarySearch(numbers, num)) System.out.println(1);
-			else System.out.println(0);
-		}
-	}
-	
-	public static boolean binarySearch(int[] numbers, int num) {
-		int start = 0;
-		int end = numbers.length-1;
-		int mid;
-		
-		while(start <= end) {
-			mid = (start + end) / 2;
-			if(numbers[mid] > num) {
-				end = mid - 1;
-			}
-			else if(numbers[mid] < num) {
-				start = mid + 1;
-			}
-			else {
-				return true;
-			}
-		}
-		return false;
-	}
+        for (int i = 0; i < guess.length; i++) {
+            if (binarySearch(guess[i])) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        }
+    }
+
+    private static boolean binarySearch(int target) {
+        int start = 0;
+        int end = N - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (A[mid] < target) {
+                start = mid + 1;
+            } else if (A[mid] > target) {
+                end = mid - 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
