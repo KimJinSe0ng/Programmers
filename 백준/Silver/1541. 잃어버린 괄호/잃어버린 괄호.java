d9d2b2669074ac508b3b4c6ed36b567c7f386f84
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-27 17:10:43 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 120 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,37 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int answer = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        String[] str = st.nextToken().split("-");
-
+        String example = st.nextToken();
+        String[] str = example.split("-");
         for (int i = 0; i < str.length; i++) {
-            int temp = mySum(str[i]);
+            int temp = sum(str[i]);
             if (i == 0) {
-                answer = answer + temp; //가장 앞에 있는 값만 더함
+                answer = answer + temp;
             } else {
-                answer = answer - temp; //뒷 부분은 더한 값들을 뺌
+                answer = answer - temp;
             }
         }
 
         System.out.println(answer);
     }
 
-    static int mySum(String a) { //나뉜 그룹의 더하기 연산 수행 함수
+    private static int sum(String s) {
         int sum = 0;
-        String[] temp = a.split("[+]");
-
+        String[] temp = s.split("[+]");
         for (int i = 0; i < temp.length; i++) {
             sum += Integer.parseInt(temp[i]);
         }
-
         return sum;
     }
 }
