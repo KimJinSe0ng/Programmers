a5c39d07adc03e70b2472fcfc87a10321ee7511a
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-27 16:25:23 +0900

    [Gold IV] Title: 수 묶기, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Gold/1744. 수 묶기/수 묶기.java
+++ 백준/Gold/1744. 수 묶기/수 묶기.java
@@ -1,24 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
+    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        PriorityQueue<Integer> plusPq = new PriorityQueue<>(Collections.reverseOrder()); //양수는 내림차순 정렬
+        N = Integer.parseInt(st.nextToken());
+        PriorityQueue<Integer> plusPq = new PriorityQueue<>(Collections.reverseOrder());
         PriorityQueue<Integer> minusPq = new PriorityQueue<>();
-        int one = 0; //1의 개수 카운트
-        int zero = 0; //0의 개수 카운트
+        int one = 0;
+        int zero = 0;
 
-        for (int i = 0; i < N; i++) { //4개의 그룹으로 분리해 저장하기
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             int data = Integer.parseInt(st.nextToken());
             if (data > 1) {
@@ -33,30 +27,30 @@ public class Main {
         }
 
         int sum = 0;
-        //양수 처리하기
+
         while (plusPq.size() > 1) {
-            int first = plusPq.remove();
-            int second = plusPq.remove();
-            sum = sum + first * second;
-        }
-        if (!plusPq.isEmpty()) { //양수 1개 남은거 처리
-            sum = sum + plusPq.remove();
+            int first = plusPq.poll();
+            int second = plusPq.poll();
+            sum += (first * second);
         }
 
-        //음수 처리하기
-        while (minusPq.size() > 1) {
-            int first = minusPq.remove();
-            int second = minusPq.remove();
-            sum = sum + first * second;
+        if (!plusPq.isEmpty()) {
+            sum += plusPq.poll();
         }
+
+        while (minusPq.size() > 1) {
+            int first = minusPq.poll();
+            int second = minusPq.poll();
+            sum += (first * second);
+        }
+
         if (!minusPq.isEmpty()) {
             if (zero == 0) {
-                sum = sum + minusPq.remove();
+                sum += minusPq.poll();
             }
         }
 
-        //1 처리하기
-        sum = sum + one;
+        sum += one;
         System.out.println(sum);
     }
 }
