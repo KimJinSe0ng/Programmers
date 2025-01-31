9088c850b953e7fec889882e1e792f28010a89c8
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-31 14:55:52 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -3,22 +3,26 @@ import java.util.*;
 
 public class Main {
     static int N, K;
+    static int[][] D;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
+        D = new int[N + 1][N + 1];
 
-        long result = factorial(N) / (factorial(K) * factorial(N - K));
-        
-        System.out.println(result);
-    }
-
-    private static long factorial(int n) {
-        if (n == 0) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
+        for (int i = 0; i <= N; i++) {
+            D[i][1] = i; // i개 중 1개를 뽑는 경우의 수는 i개
+            D[i][i] = 1; // i개 중 i개를 선택하는 경우의 수는 1개
+            D[i][0] = 1; // i개 중 1개도 선택하지 않는 경우의 수는 1개
         }
+
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+            }
+        }
+
+        System.out.println(D[N][K]);
     }
 }
