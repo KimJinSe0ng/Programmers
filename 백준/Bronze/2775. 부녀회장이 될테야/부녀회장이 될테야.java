6d9eec247305bbb26f499491ab622002171b7e89
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-31 15:35:40 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -3,30 +3,32 @@ import java.util.*;
 
 public class Main {
     static int T;
-    static int[][] apartment;
+    static int N, K;
+    static int[][] D;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         T = Integer.parseInt(st.nextToken());
-        apartment = new int[15][15];
+        D = new int[15][15]; // 0층~14층
+
         for (int i = 1; i <= 14; i++) {
-            apartment[0][i] = i;
+            D[i][1] = 1;
+            D[0][i] = i;
         }
 
         for (int i = 1; i <= 14; i++) {
-            for (int j = 1; j <= 14; j++) {
-                apartment[i][j] = apartment[i][j - 1] + apartment[i - 1][j];
+            for (int j = 2; j <= 14; j++) {
+                D[i][j] = D[i][j - 1] + D[i - 1][j];
             }
         }
 
-        for (int i = 0; i < T; i++) {
+        for (int tc = 0; tc < T; tc++) {
             st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
-
+            N = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            System.out.println(apartment[k][n]);
+            K = Integer.parseInt(st.nextToken());
+            System.out.println(D[N][K]);
         }
     }
 }
