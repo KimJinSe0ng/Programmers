7344c67a14eb039899620c700f414bfbb4db6a3e
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-01-31 15:17:02 +0900

    [Silver II] Title: 이항 계수 2, Time: 120 ms, Memory: 18260 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -1,22 +1,28 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N, K;
     static int[][] D;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        K = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
         D = new int[N + 1][N + 1];
+
         for (int i = 0; i <= N; i++) {
-            D[i][1] = i; //i개에서 1개 선택하는 경우의 수는 1개
-            D[i][0] = 1; //i개에서 1개도 선택하지 않는 경우의 수는 1개
-            D[i][i] = 1; //i개에서 모두 선택하는 경우의 수는 1개
+            D[i][1] = i; // i개 중 1개를 뽑는 경우의 수는 i개
+            D[i][i] = 1; // i개 중 i개를 선택하는 경우의 수는 1개
+            D[i][0] = 1; // i개 중 1개도 선택하지 않는 경우의 수는 1개
         }
 
         for (int i = 2; i <= N; i++) {
-            for (int j = 1; j < i; j++) { //고르는 수의 개수가 전체 개수를 넘을 수 없음
-                D[i][j] = (D[i - 1][j] + D[i - 1][j - 1]) % 10007;
+            for (int j = 1; j < i; j++) {
+                D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+                D[i][j] %= 10007;
             }
         }
 
