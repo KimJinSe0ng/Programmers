b4491a4f82039c687ccf0b5bb7cf19eb75e2b3e3
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-02-04 14:54:18 +0900

    [Silver V] Title: 다리 놓기, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,26 +1,30 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static long[][] D;
-    public static void main(String[] args) {
-        D = new long[31][31];
-        for (int i = 0; i <= 30; i++) {
-            D[i][1] = i; //i개 중 1개를 뽑는 경우의 수는 i개
-            D[i][0] = 1; //i개 중 1개도 선택하지 않는 경우의 수는 1개
-            D[i][i] = 1; //i개 중 i개를 선택하는 경우의 수는 1개
+    static int T;
+    static int[][] D;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        T = Integer.parseInt(st.nextToken());
+        D = new int[31][31];
+        for (int i = 0; i < 31; i++) {
+            D[i][1] = i;
+            D[i][0] = 1;
+            D[i][i] = 1;
         }
 
-        for (int i = 2; i <= 30; i++) {
-            for (int j = 1; j < i; j++) { //고르는 수의 개수가 전체 개수를 넘을 수 없음
+        for (int i = 2; i < 31; i++) {
+            for (int j = 1; j < 31; j++) {
                 D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
             }
         }
 
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        for (int i = 0; i < t; i++) {
-            int N = sc.nextInt();
-            int M = sc.nextInt();
+        for (int tc = 0; tc < T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
             System.out.println(D[M][N]);
         }
     }
