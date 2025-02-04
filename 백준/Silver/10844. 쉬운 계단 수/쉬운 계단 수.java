f80275e0c9ad9cec68bdfb99684aef8e73978b2b
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-02-04 21:54:15 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 108 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,46 +1,33 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+import java.util.*;
+
 public class Main {
-	
-	final static long mod = 1000000000;
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		long[][] dp = new long[N + 1][10];
-		
-		for(int i = 1; i < 10; i++) {
-			dp[1][i] = 1; 
-		}
-		
-		for(int i = 2; i <= N; i++) {
-			
-			for(int j = 0; j < 10; j++) {
-				
-				if(j == 0) {
-					dp[i][0] = dp[i - 1][1] % mod;
-				}
-				else if (j == 9) {
-					dp[i][9] = dp[i - 1][8] % mod;
-				}
-				else {
-					dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
-				}
-			}
-		}
-		
-		long result = 0;
-		
-		for(int i = 0; i < 10; i++) {
-			result += dp[N][i];
-		}
-		
-		System.out.println(result % mod);
-	}
- 
+    static int N;
+    static long[][] D;
+    static long mod = 1_000_000_000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        D = new long[N + 1][10];
+
+        for (int i = 1; i < 10; i++) {
+            D[1][i] = 1;
+        }
+
+        for (int i = 2; i <= N; i++) {
+            D[i][0] = D[i - 1][1];
+            D[i][9] = D[i - 1][8];
+            for (int j = 1; j <= 8; j++) {
+                D[i][j] = (D[i - 1][j - 1] + D[i - 1][j + 1]) % mod;
+            }
+        }
+
+        long sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum = (sum + D[N][i]) % mod;
+        }
+
+        System.out.println(sum);
+    }
 }
