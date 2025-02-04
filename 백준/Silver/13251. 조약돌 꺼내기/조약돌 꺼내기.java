2ac7a217e2d6ccbb122a41b913d3a1e4ec5cffcf
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-02-04 15:19:25 +0900

    [Silver III] Title: 조약돌 꺼내기, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.java
+++ 백준/Silver/13251. 조약돌 꺼내기/조약돌 꺼내기.java
@@ -1,37 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int M, K, T;
+    static int[] D = new int[51];
+    static double[] probability = new double[51];
+    static double answer;
     public static void main(String[] args) throws IOException {
-        int M, K, T;
-        int[] D = new int[51];
-        double[] probability = new double[51];
-        double ans;
-        T = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         M = Integer.parseInt(st.nextToken());
-        
         st = new StringTokenizer(br.readLine());
+        T = 0; // 전체 조약돌의 개수
         for (int i = 0; i < M; i++) {
             D[i] = Integer.parseInt(st.nextToken());
             T += D[i];
         }
-
         st = new StringTokenizer(br.readLine());
         K = Integer.parseInt(st.nextToken());
-        ans = 0.0;
+        answer = 0.0;
         for (int i = 0; i < M; i++) {
+            // 선택 조약돌 개수보다 현재 색 조약돌 개수가 적으면 모두 같은 색으로 뽑을 확률은 0
             if (D[i] >= K) {
                 probability[i] = 1.0;
                 for (int j = 0; j < K; j++) {
+                    // i 색깔을 모두 뽑을 확률 = i 색깔을 모두 뽑을 확률 * 현재 색깔 개수 - j / 전체 색깔 개수 - j;
                     probability[i] *= (double) (D[i] - j) / (T - j);
                 }
-                ans += probability[i];
+                answer += probability[i];
             }
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
