3ce6c4460f9bcdbe3fc04e6217410706ae68c303
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-02-04 21:29:32 +0900

    [Silver III] Title: 이친수, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
@@ -9,16 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         D = new long[N + 1][2];
-        D[1][1] = 1; //1자리 이친수는 1 한 가지
+
+        D[1][1] = 1;
         D[1][0] = 0;
 
         for (int i = 2; i <= N; i++) {
             D[i][0] = D[i - 1][1] + D[i - 1][0];
             D[i][1] = D[i - 1][0];
         }
+
         System.out.println(D[N][0] + D[N][1]);
     }
 }
