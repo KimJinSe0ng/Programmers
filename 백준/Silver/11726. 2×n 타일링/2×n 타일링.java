9be8355fd2a53897d686f26f329ff87e1b48f743
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-02-04 21:40:01 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,22 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
-    static int[] dp;
+    static long[] D;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
-        dp = new int[1001];
-        dp[1] = 1;
-        dp[2] = 2;
+        D = new long[1001];
+
+        D[1] = 1;
+        D[2] = 2;
+
         for (int i = 3; i <= N; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+            D[i] = (D[i - 2] + D[i - 1]) % 10007;
         }
-        System.out.println(dp[N]);
+
+        System.out.println(D[N]);
     }
 }
