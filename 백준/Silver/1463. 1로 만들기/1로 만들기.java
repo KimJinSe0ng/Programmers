d029097e9c94a1498be3ac488587724e100fa8ae
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-02-04 16:33:58 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18080 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -3,24 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main { //1463
-    static int n;
-    static int[] D;
+public class Main {
+    static int N;
+    static int[] D; // D[i] : i에서 1로 만드는 데 걸리는 최소 연산 횟수
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        D = new int[n + 1];
+        N = Integer.parseInt(st.nextToken());
+        D = new int[N + 1];
         D[1] = 0;
-        for (int i = 2; i <= n; i++) {
-            D[i] = D[i - 1] + 1; //-1연산
-            if (i % 2 == 0) {
-                D[i] = Math.min(D[i], D[i / 2] + 1); //나누기 2 연산
-            }
-            if (i % 3 == 0) {
-                D[i] = Math.min(D[i], D[i / 3] + 1); //나누기 3연산
-            }
+        for (int i = 2; i <= N; i++) {
+            D[i] = D[i - 1] + 1;
+            if (i % 2 == 0) D[i] = Math.min(D[i], D[i / 2] + 1);
+            if (i % 3 == 0) D[i] = Math.min(D[i], D[i / 3] + 1);
         }
-        System.out.println(D[n]);
+        System.out.println(D[N]);
     }
 }
