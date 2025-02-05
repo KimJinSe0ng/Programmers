4379429e78c06fe04bab2a2d0fbcc5324017c870
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-02-05 11:44:02 +0900

    [Gold V] Title: 연속합 2, Time: 268 ms, Memory: 25716 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -1,58 +1,43 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static int N;
+    static int[] A;
+    static int[] L;
+    static int[] R;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
+        // 오른쪽 방향으로 index를 포함한 최대 연속 합 구하기
+        L = new int[N];
+        L[0] = A[0];
+        int result = L[0];
+        for (int i = 1; i < N; i++) {
+            L[i] = Math.max(A[i], L[i - 1] + A[i]);
+            result = Math.max(result, L[i]); // 1개도 제거하지 않았을 때를 기본 최댓값으로 저장
+        }
 
-		int N = Integer.parseInt(br.readLine());
-		int[] arr = new int[N];
+        // 왼쪽 방향으로 index를 포함한 최대 연속 합 구하기
+        R = new int[N];
+        R[N - 1] = A[N - 1];
+        for (int i = N - 2; i >= 0; i--) {
+            R[i] = Math.max(A[i], R[i + 1] + A[i]);
+        }
 
-		st = new StringTokenizer(br.readLine());
+        // L[i-1] + R[i+1] 2개의 구간 합 배열을 더하면 i번째 값을 제거한 효과를 얻음
+        for (int i = 1; i < N - 1; i++) {
+            int temp = L[i - 1] + R[i + 1];
+            result = Math.max(result, temp);
+        }
 
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-
-		int[] dp1 = new int[N];
-		dp1[0] = arr[0];
-
-		int ans = dp1[0];
-
-		// 오른쪽 방향으로 최대 연속 합을 각각 저장함.
-		for (int i = 1; i < N; i++) {
-			dp1[i] = Math.max(dp1[i - 1] + arr[i], arr[i]);
-
-			ans = Math.max(ans, dp1[i]);
-		}
-
-		int[] dp2 = new int[N];
-		dp2[N - 1] = arr[N - 1];
-
-		// 왼쪽 방향으로 최대 연속 합을 각각 저장함.
-		for (int i = N - 2; i >= 0; i--) {
-			dp2[i] = Math.max(dp2[i + 1] + arr[i], arr[i]);
-		}
-
-		// 특정 값을 지웠다고 가정하고, 그 값의 오른쪽 방향과 왼쪽 방향의 최대 연속 합을 더함.
-		// 윗줄에서 구한 값과 ans를 비교하여 더 큰 값으로 갱신.
-		for (int i = 1; i < N - 1; i++) {
-			int temp = dp1[i - 1] + dp2[i + 1];
-
-			ans = Math.max(ans, temp);
-		}
-
-		bw.write(ans + "\n");
-		bw.flush();
-		bw.close();
-		br.close();
-	}
-
+        System.out.println(result);
+    }
 }
