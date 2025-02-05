9a0e9ba46ef8a59da54b19b18e3533932c5d9881
KimJinSeong <118241990+KimJinSe0ng@users.noreply.github.com>
2025-02-05 23:21:05 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1108 ms, Memory: 79088 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,48 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int N;
-    static ArrayList<Integer>[] tree;
-    static int[] parent;
-
+    static List<Integer>[] graph;
+    static boolean[] visited;
+    static int[] par;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
-        tree = new ArrayList[N + 1];
-        parent = new int[N + 1];
+        graph = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        par = new int[N + 1];
 
-        for (int i = 1; i < tree.length; i++) {
-            tree[i] = new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < N - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
-            tree[s].add(e);
-            tree[e].add(s);
+            graph[s].add(e);
+            graph[e].add(s);
         }
 
-        dfs(1, 0);
+        dfs(1);
 
         for (int i = 2; i <= N; i++) {
-            System.out.println(parent[i]);
+            System.out.println(par[i]);
         }
     }
 
-    public static void dfs(int node, int prev) {
-        for (int next : tree[node]) {
-            if (next == prev) { // 역주행 제한
-                continue;
+    private static void dfs(int node) {
+        visited[node] = true;
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                par[next] = node;
+                dfs(next);
             }
-            parent[next] = node; 
-            dfs(next, node); 
         }
     }
 }
